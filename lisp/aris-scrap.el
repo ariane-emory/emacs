48322a061aee662964353ec78e82c2cfafc0e792
Ariane Emory <ariane.emory@protonmail.com>
2024-03-30 19:59:29 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -10,11 +10,16 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Test defining a function:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Reset the pattern dispatch table first so that we won't get
+;; 'Pattern blah already defined' errors if we evaluate this
+;; buffer twice:
 (setq *lust-style-def--pattern-dispatch-table* nil)
+
 ;; Insertion order needs to be reversed for now. Fix this!
 (def (fib n) (+ (fib (- n 1)) (fib (- n 2)))) 
 (def (fib 1) 1)
 (def (fib 0) 0)
+
 (setq final (fib 10))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
