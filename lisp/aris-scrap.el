772b3a935d4367c6e176323052805e5308b07ba9
Ariane Emory <ariane.emory@protonmail.com>
2024-03-30 20:37:51 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -13,17 +13,22 @@
 ;; Reset the pattern dispatch table first so that we won't get
 ;; 'Pattern blah already defined' errors if we evaluate this
 ;; buffer twice:
-(setq *lust-style-def--pattern-dispatch-table* nil)
+(progn
+  (setq *lust-style-def--pattern-dispatch-table* nil)
 
-(def (fib 0) 0)
-(def (fib 1) 1)
-(def (fib n) (+ (fib (- n 1)) (fib (- n 2)))) 
-(def w 8)
-(def x (1+ w))
-(def y '(w x))
-(def z (list w x))
+  (def w 8)
+  (def x (1+ w))
+  (def y '(w x))
+  (def z (list w x))
+  (def (fib 0) 0)
+  (def (fib 1) 1)
+  (def (fib n) (+ (fib (- n 1)) (fib (- n 2)))) 
 
-(setq final (fib 10))
+  (setq result
+    (list
+      (fib (car z))
+      (fib (cadr z))
+      (fib 10)))) ;; => (21 34 55)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
