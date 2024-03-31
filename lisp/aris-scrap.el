9017eac4e78920d966ec79cbc7432fe26a02d212
Ariane Emory <ariane.emory@protonmail.com>
2024-03-31 00:49:58 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -33,6 +33,45 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'dash)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun mapr (lst fn)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Map FN over LST."
+  (mapcar fn lst))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(cl-defun merge-duplicate-alist-keys (alist &optional (use-dots t))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "A helper function used by aris-match-pattern to merge the values of duplicate
+ ALIST.
+
+Example:
+(aris-match-pattern--merge-duplicate-alist-keys '((y . 22) (x . 66) (w . 3) (w .) ⇒
+  '((y . 22) (x . 66) (w 3 2 )) (v . 77))"
+  (let (result)
+    (dolist (pair alist)
+      (let* ( (key (car pair))
+              (is-dotted (-cons-pair? pair))
+              (value (if is-dotted (cdr pair) (cadr pair)))
+              (existing (assoc key result)))
+        (if existing
+          (setcdr existing (nconc (cdr existing) (list value)))
+          (push (cons key (list value)) result))))
+    (nreverse
+      (if (not use-dots)
+        result
+        (mapr
+          result
+          (lambda (pair)
+            (if (length> (cdr pair) 1)
+              pair
+              (cons (car pair) (cadr pair)))))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (merge-duplicate-alist-keys '((v . 1) (w . 2) (w . 3) (x . 4) (y . 5) (y . 6) (y . 7) (z . 8)) nil)
 (merge-duplicate-alist-keys '((v . 1) (w . 2) (w . 3) (x . 4) (y . 5) (y . 6) (y . 7) (z . 8)) t)
