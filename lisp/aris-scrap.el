b6cf0166e2c623ced73f2bd4fd556d168ede1fec
Ariane Emory <ariane.emory@protonmail.com>
2024-03-30 23:12:49 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -8,30 +8,31 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'aris-funs--lust-style-syntax)
-(begin
-  ;; Test defining a function:
-  ;; Reset the pattern dispatch table first so that we won't get
-  ;; 'Pattern blah already defined' errors if we evaluate this
-  ;; buffer twice:
-  (setq *lust-style-syntax--pattern-dispatch-table* nil)
+(when nil
+  (require 'aris-funs--lust-style-syntax)
+  (begin
+    ;; Test defining a function:
+    ;; Reset the pattern dispatch table first so that we won't get
+    ;; 'Pattern blah already defined' errors if we evaluate this
+    ;; buffer twice:
+    (setq *lust-style-syntax--pattern-dispatch-table* nil)
 
-  (def w 8)
-  (def x (1+ w))
-  (def y '(w x))
-  (def z (list w x))
-  (def (fib 0) 0)
-  (def (fib 1) 1)
-  (def (fib n) (+ (fib (- n 1)) (fib (- n 2)))) 
+    (def w 8)
+    (def x (1+ w))
+    (def y '(w x))
+    (def z (list w x))
+    (def (fib 0) 0)
+    (def (fib 1) 1)
+    (def (fib n) (+ (fib (- n 1)) (fib (- n 2)))) 
 
-  (def result
-    (list
-      (fib (car z))
-      (fib (cadr z))
-      (fib 10)))) ;; => (21 34 55)
+    (def result
+      (list
+        (fib (car z))
+        (fib (cadr z))
+        (fib 10))))) ;; => (21 34 55)
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  ;; scratch ends here:
+;; scratch ends here:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
