16c77e66ef6a793b482a99a3c4cefb3f8b24c1ab
Ariane Emory <ariane.emory@protonmail.com>
2024-04-02 14:22:11 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -9,45 +9,46 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when nil
-  ;; Test defining a function:
-  ;; Reset the pattern dispatch table first so that we won't get
-  ;; 'Pattern blah already defined' errors if we evaluate this
-  ;; buffer twice:
-  (setq *lust-style-syntax--pattern-dispatch-table* nil)
-  ;;(setq *lust-style-syntax--verbose* nil)
+  (progn
+    ;; Test defining a function:
+    ;; Reset the pattern dispatch table first so that we won't get
+    ;; 'Pattern blah already defined' errors if we evaluate this
+    ;; buffer twice:
+    (setq *lust-style-syntax--pattern-dispatch-table* nil)
+    ;;(setq *lust-style-syntax--verbose* nil)
 
-  (def p 6)
-  (def w 12)
-  (def p 18)
-  (def x (1+ w))
-  (def y '(w x))
-  (def z (list w x))
-  (def (fib 0) 0)
-  (def (fib 1) 1)
-  (def (fib n) (+ (fib (- n 1)) (fib (- n 2))))
-  (def qqq 444)
-  (def qqq '(333 444))
-  (def (double n) (+ n n))
+    (def p 6)
+    (def w 12)
+    (def p 18)
+    (def x (1+ w))
+    (def y '(w x))
+    (def z (list w x))
+    (def (fib 0) 0)
+    (def (fib 1) 1)
+    (def (fib n) (+ (fib (- n 1)) (fib (- n 2))))
+    (def qqq 444)
+    (def qqq '(333 444))
+    (def (double n) (+ n n))
 
 
-  ;; (message "%s" (fib 0))
-  ;; (message "%s" (fib 1))
-  ;; (message "%s" (fib 2))
-  ;; (message "%s" (fib 3))
-  ;; (message "%s" (fib 4))
-  ;; (message "%s" (fib 5))
-  ;; (message "%s" (fib 6))
-  ;; (message "%s" (fib 7))
-  ;; (message "%s" (fib 8))
-  ;; (message "%s" (fib 9))
-  ;; (message "%s" (fib 10))
+    ;; (message "%s" (fib 0))
+    ;; (message "%s" (fib 1))
+    ;; (message "%s" (fib 2))
+    ;; (message "%s" (fib 3))
+    ;; (message "%s" (fib 4))
+    ;; (message "%s" (fib 5))
+    ;; (message "%s" (fib 6))
+    ;; (message "%s" (fib 7))
+    ;; (message "%s" (fib 8))
+    ;; (message "%s" (fib 9))
+    ;; (message "%s" (fib 10))
 
-  (let ((*lust-style-syntax--verbose* nil))
-    (def result
-      (list
-        (fib 4) ;; (car z))
-        (fib 6);; (cadr z))
-        (fib 8))))) ;; 10)))
+    (let ((*lust-style-syntax--verbose* nil))
+      (def result
+        (list
+          (fib 4) ;; (car z))
+          (fib 6);; (cadr z))
+          (fib 8)))))) ;; 10)))
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
