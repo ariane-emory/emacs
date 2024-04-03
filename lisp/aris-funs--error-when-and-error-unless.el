045482785e85321007a17faeb8c8cc1eba36377d
Ariane Emory <ariane.emory@protonmail.com>
2024-04-03 17:18:59 -0400

    ...

--- lisp/aris-funs--error-when-and-error-unless.el
+++ lisp/aris-funs--error-when-and-error-unless.el
@@ -57,38 +57,33 @@ Unacceptable case:
   "
   (unless format-args-and-body
     (error "error-when: No body provided."))
-
   (let* ( (string-is-format-string
             (string-is-format-string-p error-message))
           (body
-            (if string-is-format-string
-              (cdr format-args-and-body)
-              format-args-and-body))
+            (if string-is-format-string (cdr format-args-and-body) format-args-and-body))
           (format-args
             (when string-is-format-string
-              `(list ,@(cadar format-args-and-body)))))
+              (message "format-args-and-body: %s" format-args-and-body)
+              (message "car   format-args-and-body: %s" (car format-args-and-body))
+              (message "cdar  format-args-and-body: %s" (cdar format-args-and-body))
+              (message "cadar format-args-and-body: %s" (cadar format-args-and-body))
+
+              (let ((unquoted-format-args (cadar format-args-and-body)))
+                `(list ,@unquoted-format-args)))))
     `(let ((it (progn ,@body)))
        (when it
          (apply #'error ,error-message ,format-args)))))
+         ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when nil
   (error-when "This should raise an error because condition is non-nil: %s %s %s" '(it 8 (+ 2 3)) 1 nil "THIS STRING IS TRUE")
   (error-when "This should NOT raise an error because condition is nil: %s %s %s" '(it 8 (+ 2 3)) 1 nil nil)
   (error-when "This should raise an error because condition is non-nil." 1 nil "THIS STRING IS TRUE")
   (error-when "This should NOT raise an error because condition is nil." 1 nil nil)
-
   )
-;; (if (not (string-is-format-string-p error-message))
-;;   (let ((body `(progn ,@format-args-and-body)))
-;;     `(let ((it ,body))
-;;        (when ,body
-;;          (error ,error-message))))
-;;   (let ( (body `(progn ,@(cdr format-args-and-body)))
-;;          (format-args `(list ,@(cadar format-args-and-body))))
-;;     `(let ((it ,body))
-;;        (when it
-;;          (apply #'error ,error-message ,format-args))))))
-           ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
