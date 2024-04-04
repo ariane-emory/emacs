159f0788252321bb9f89e91966fd795956163900
Ariane Emory <ariane.emory@protonmail.com>
2024-04-04 16:02:31 -0400

    wip

--- lisp/aris-funs--pipe.el
+++ lisp/aris-funs--pipe.el
@@ -3,7 +3,40 @@
 (require 'cl-lib)
 (require 'aris-funs--pattern-dispatch)
 (require 'aris-funs--unsorted)
+(require 'aris-funs--with-messages)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defgroup pipe nil
+  "Elixir-style pipe operator.")
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defcustom *pipe--verbose* nil
+  "Whether the pipe operator should print verbose messages."
+  :group 'pipe
+  :type 'boolean)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defcustom *pipe--print-fun* 'indented-message
+  "The function to use to print messages."
+  :group 'pipe
+  :type 'function)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro pipe--print (first &rest rest)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Wrap *pipe--print-fun*"
+  `(when *pipe--verbose*     
+     (funcall *pipe--print-fun* ,first ,@rest)
+     nil))
+     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -20,57 +53,85 @@
           (init-form (if head-is-spec (cadar head) head))
           (body tail))
 
-    (prn (make-string 80 ?\=))
-    (prn "PIPE CALLED")
-    (prn (make-string 80 ?\=))
-    (prn "head: %s" head)
-    (prn "head-is-spec: %s" head-is-spec)
-    (prn "var: %s" var)
-    (prn "init-form: %s" init-form)
-    (prn "body: %s" body)
+    (pipe--print (make-string 80 ?\=))
+    (pipe--print "PIPE CALLED")
+    (pipe--print (make-string 80 ?\=))
+    (pipe--print "head: %s" head)
+    (pipe--print "head-is-spec: %s" head-is-spec)
+    (pipe--print "var: %s" var)
+    (pipe--print "init-form: %s" init-form)
+    (pipe--print "body: %s" body)
 
     `(progn
-       (prn (make-string 80 ?\=))
+       (pipe--print (make-string 80 ?\=))
        (let ( (last ,init-form)
               (sym ',var)
               (,var nil))
          (catch 'return
            (mapcr ',body
              (lambda (expr)
-               (prn (make-string 80 ?\=))
-               (prn "Expr: %S" expr)
-               (prn "Var:  %S" ,var)
-               (prn "Last: %S" last)
+               (pipe--print (make-string 80 ?\=))
+               (pipe--print "Expr: %S" expr)
+               (pipe--print "Var:  %S" ,var)
+               (pipe--print "Last: %S" last)
 
                (cl-flet ((expr-fun
                            `(lambda (sym)
                               (cl-flet ((return (,sym) (throw 'return ,sym)))
-                                ;;(prn "Eval: %S" ',expr)
+                                ;;(pipe--print "Eval: %S" ',expr)
                                 (let ((result ,expr))
-                                  ;;(prn "Next: %S" result)
+                                  ;;(pipe--print "Next: %S" result)
                                   result)))))
                  (cond
                    ((eq expr '->)
                      (setq ,var last)
                      (setq last nil)
-                     (prn "Updated by arrow! Var is %S, last is %S" ,var last)
+                     (pipe--print "Updated by arrow! Var is %S, last is %S" ,var last)
                      )
                    (t (setq last (expr-fun ,var))
-                     (prn "Updated by call! Var is %S, last is %S" ,var last))))))
+                     (pipe--print "Updated by call! Var is %S, last is %S" ,var last))))))
            (throw 'return
              (progn
-               (prn (make-string 80 ?\=))
-               (prn "Returning: %S" (or last ,var))
-               (prn (make-string 80 ?\=))
-               ;;(prn "Expr: %S" expr)
-               (prn "Var:  %S" ,var)
-               (prn "Last: %S" last)
+               (pipe--print (make-string 80 ?\=))
+               (pipe--print "Returning: %S" (or last ,var))
+               (pipe--print (make-string 80 ?\=))
+               ;;(pipe--print "Expr: %S" expr)
+               (pipe--print "Var:  %S" ,var)
+               (pipe--print "Last: %S" last)
                (or last ,var))))))))
                ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(let ((*with-messages--depth-indicator-enable*))
+(defmacro plist-put! (plist-symbol key value)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Add or updated a key-value pair in a plist."
+  `(setq ,plist-symbol (plist-put ,plist-symbol ,key ,value)))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro plist-remove!(plist-symbol key)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Remove a key and it's associated value - this actually removes it, it doesn't just set it to nill"
+  `(let (new-plist)
+     (dolist (kvp ,plist-symbol)
+       (unless (eq (car kvp) ,key)
+         (plist-put! new-plist (car kvp) (cdr kvp))))
+     (setq ,plist-symbol new-plist)))
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(setq plist nil)
+(plist-put! plist 'a 1) 
+(plist-put! plist 'b 2)
+(plist-put! plist 'c 3)
+(plist-remove! plist 'b)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(let ( (*pipe--verbose* nil)
+       (*with-messages--depth-indicator-enable* nil))
   ;; Do some simple arithmetic with a pipe:
   (|> 2 -> (+ _ 1) -> (* 3 _)) ;; ⇒ 9
 
@@ -81,17 +142,17 @@
   (def (fib 0) 0)
   (def (fib 1) 1)
   (def (fib n)
-    (|> (prn "Calculating (fib %d) using a pipe-based fib..." n)
+    (|> (pipe--print "Calculating (fib %d) using a pipe-based fib..." n)
       (|> n -> (- _ 1) -> (fib _)) ->
       (+ _ (|> n -> (- _ 2) -> (fib _) ->
-             (prn "Calculated (fib %d) = %d" n _) _))))
+             (pipe--print "Calculated (fib %d) = %d" n _) _))))
 
   ;; Call it with some output commenting on the proceedings:
   (|>
-    3 -> (prn "Starting out with %d" _) (+ _ (|> 2 -> (+ _ 5))) ->
-    (prn "Getting the result of (fib %d)" _) (fib _) ->
+    3 -> (pipe--print "Starting out with %d" _) (+ _ (|> 2 -> (+ _ 5))) ->
+    (pipe--print "Getting the result of (fib %d)" _) (fib _) ->
     "I'm just a harmless string sitting in the pipe doing doing nothing."
-    (prn "Result =  %d" _) _)) ;; ⇒ 55
+    (pipe--print "Result =  %d" _) _)) ;; ⇒ 55
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
