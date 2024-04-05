1b08140da979cf6f4f9c6d3546141c6d7a339ec5
Ariane Emory <ariane.emory@protonmail.com>
2024-04-05 18:25:18 -0400

    ...

--- lisp/aris-funs--pipe.el
+++ lisp/aris-funs--pipe.el
@@ -109,36 +109,67 @@
        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro |> (head &rest tail)
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "`pipe' with optional let-like binding/symbol naming."
-  (let* ((head-is-spec
-           (and
-             (consp head)
-             (consp (car head))
-             (length> (car head) 0)
-             (length< (car head) 3)))
-          (var (if head-is-spec (caar head) *pipe--default-var-sym*))
-          (init-form (if head-is-spec (cadar head) head))
-          (body tail))
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (defmacro |> (head &rest tail)
+;;   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;   "`pipe' with optional let-like binding/symbol naming."
+;;   (let* ((head-is-spec
+;;            (and
+;;              (consp head)
+;;              (consp (car head))
+;;              (length> (car head) 0)
+;;              (length< (car head) 3)))
+;;           (var (if head-is-spec (caar head) *pipe--default-var-sym*))
+;;           (init-form (if head-is-spec (cadar head) head))
+;;           (body tail))
 
-    (pipe--print (make-string 80 ?\=))
-    (pipe--print "PIPE CALLED")
-    (pipe--print (make-string 80 ?\=))
-    (pipe--print "head: %s" head)
-    (pipe--print "head-is-spec: %s" head-is-spec)
-    (pipe--print "var: %s" var)
-    (pipe--print "init-form: %s" init-form)
-    (pipe--print "body: %s" body)
+;;     (pipe--print (make-string 80 ?\=))
+;;     (pipe--print "PIPE CALLED")
+;;     (pipe--print (make-string 80 ?\=))
+;;     (pipe--print "head: %s" head)
+;;     (pipe--print "head-is-spec: %s" head-is-spec)
+;;     (pipe--print "var: %s" var)
+;;     (pipe--print "init-form: %s" init-form)
+;;     (pipe--print "body: %s" body)
 
-    `(progn
-       (pipe--print (make-string 80 ?\=))
-       (let ( (last ,init-form)
-              (sym ',var)
-              (,var nil))
-         ))))
-               ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;     `(progn
+;;        (pipe--print (make-string 80 ?\=))
+;;        (let ( (last ,init-form)
+;;               (sym ',var)
+;;               (,var nil))
+;;          (catch 'return
+;;            (mapcr ',body
+;;              (lambda (expr)
+;;                (pipe--print (make-string 80 ?\=))
+;;                (pipe--print "Expr: %S" expr)
+;;                (pipe--print "Var:  %S" ,var)
+;;                (pipe--print "Last: %S" last)
+
+;;                (cl-flet ((expr-fun
+;;                            `(lambda (sym)
+;;                               (cl-flet ((return (,sym) (throw 'return ,sym)))
+;;                                 ;;(pipe--print "Eval: %S" ',expr)
+;;                                 (let ((result ,expr))
+;;                                   ;;(pipe--print "Next: %S" result)
+;;                                   result)))))
+;;                  (cond
+;;                    ((eq expr '->)
+;;                      (setq ,var last)
+;;                      (setq last nil)
+;;                      (pipe--print "Updated by arrow! Var is %S, last is %S" ,var last)
+;;                      )
+;;                    (t (setq last (expr-fun ,var))
+;;                      (pipe--print "Updated by call! Var is %S, last is %S" ,var last))))))
+;;            (throw 'return
+;;              (progn
+;;                (pipe--print (make-string 80 ?\=))
+;;                (pipe--print "Returning: %S" (or last ,var))
+;;                (pipe--print (make-string 80 ?\=))
+;;                ;;(pipe--print "Expr: %S" expr)
+;;                (pipe--print "Var:  %S" ,var)
+;;                (pipe--print "Last: %S" last)
+;;                (or last ,var))))))))
+;;                ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
