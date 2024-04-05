143f346292a64c34139cce194fa25273a5185cf0
Ariane Emory <ariane.emory@protonmail.com>
2024-04-04 21:11:27 -0400

    ...

--- lisp/aris-funs--plists.el
+++ lisp/aris-funs--plists.el
@@ -6,11 +6,35 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun make-plist (keys vals)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Build a plist from KEYS and VALS."
+  (unless (list? keys) (error "KEYS must be a list."))
+  (unless (list? vals) (error "VALS must be a list."))
+  (unless (>= (length keys) (length vals)) (error "KEYS must be at least as long as VALS."))
+  (when keys
+    (let* ( (result (list (pop keys) (pop vals)))
+            (tail (cdr result)))
+      (while keys
+        (let ((new-tail (list (pop keys) (pop vals))))
+          (setq tail (cdr (rplacd! tail new-tail)))))
+      result)))
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (setq plist (make-plist '(d a c b) '(4 1 3 2)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defmacro plist-put! (plist-symbol key value)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Add or update a key-value pair in a plist."
   `(setq ,plist-symbol (plist-put ,plist-symbol ,key ,value)))
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (setq plist (make-plist '(d a c b) '(4 1 3 2)))
+;; (plist-put! plist 'a 10)
+;; (plist-put! plist 'b 20)
+;; plist
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -26,6 +50,32 @@
          (setq old-plist (cddr old-plist))))
      (setq ,plist-symbol new-plist)))
      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (setq plist (make-plist '(d a c b) '(4 1 3 2)))
+;; (plist-remove! plist 'a)
+;; (plist-remove! plist 'b)
+;; plist
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun plist-remove (plist key)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Remove a key and it's associated value. This version returns a new plist and
+does not modify the original."
+  (let (new-plist (old-plist plist))
+    (while old-plist
+      (let ((k (car old-plist))
+             (v (cadr old-plist)))
+        (unless (eq k key)
+          (plist-put! new-plist k v))
+        (setq old-plist (cddr old-plist))))
+    new-plist))
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (setq plist (make-plist '(d a c b) '(4 1 3 2)))
+;; (plist-remove plist 'a)
+;; (plist-remove (plist-remove plist 'c) 'b)
+;; plist
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -38,11 +88,30 @@
                collect (list key value))
          (lambda (a b) (string< (car a) (car b)))))))
          ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (setq plist (make-plist '(d a c b) '(4 1 3 2)))
+;; (plist-sort! plist)
+;; plist
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun plist-sort (plist-symbol)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Sort a plist by key. This version returns a new plist and does not modify the original."
+  (apply 'append
+    (sort (cl-loop for (key value) on plist-symbol by #'cddr
+            collect (list key value))
+      (lambda (a b) (string< (car a) (car b))))))
+      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (setq plist (make-plist '(d a c b) '(4 1 3 2)))
+;; (plist-sort plist)
+;; plist
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun plist-keys (plist)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Extracts the keys from a plist PLIST."
   (unless (listp plist) (error "PLIST must be a list"))
   (when plist
@@ -55,15 +124,22 @@
         (pop plist))
       result)))
       ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (setq plist (make-plist '(d a c b) '(4 1 3 2)))
+;; (plist-keys plist)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun plist-vals (plist)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Extracts the values from a plist PLIST."
   (unless (listp plist)          (error "PLIST must be a list"))
   (unless (even? (length plist)) (error "PLIST must have an even number of elements"))
   (plist-keys (cdr plist)))
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (setq plist (make-plist '(d a c b) '(4 1 3 2)))
+;; (plist-vals plist)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -81,24 +157,9 @@ is odd, the last cons in the resulting alist's value cell will be nil."
           (setq tail (rplacd! tail new-alist-item))))
       result)))
       ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
+;; (setq plist (make-plist '(d a c b) '(4 1 3 2)))
+;; (plist-to-alist plist)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun make-plist (keys vals)
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Build a plist from KEYS and VALS."
-  (unless (list? keys) (error "KEYS must be a list."))
-  (unless (list? vals) (error "VALS must be a list."))
-  (unless (>= (length keys) (length vals)) (error "KEYS must be at least as long as VALS."))
-  (when keys
-    (let* ( (result (list (pop keys) (pop vals)))
-            (tail (cdr result)))
-      (while keys
-        (let ((new-tail (list (pop keys) (pop vals))))
-          (setq tail (cdr (rplacd! tail new-tail)))))
-      result)))
-      ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
