91037b83e5ae8cd5dbf072bca08157cc323b3bec
Ariane Emory <ariane.emory@protonmail.com>
2024-04-06 19:55:48 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -3,7 +3,6 @@
 (require 'pp)
 (require 'cl-lib)
 (require 'aris-funs--alists)
-(require 'aris-funs--pattern-dispatch)
 (require 'aris-funs--error-when-and-error-unless)
 (require 'aris-funs--lists)
 (require 'aris-funs--pattern-dispatch)
