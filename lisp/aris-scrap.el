dda4b306e6c97dfd74f0487d4732f82885cf7df5
Ariane Emory <ariane.emory@protonmail.com>
2024-04-06 12:36:19 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -268,30 +268,34 @@
 (|> 5 (+ _ 7) :(prn  "hello") (+ _ 3) (return 99) neg :?(when (negative? _) (neg _)) :(when (> _ 20) (return 11)) 1)
 (|> 5 (+ _ 7) double :(prn "hello") (+ _ 3) (return 99) neg :?(when (negative? _) (neg _)) :(when (> _ 20) (return 11)) 1)
 
-(|> 5 (+ _ 7) double :(prn "hello") (+ _ 3) neg :when? negative? neg :when? (> _ 20) (return 11) 1)
-(|> 5 (+ _ 7) :(prn  "hello") (+ _ 3) (return 99) neg :when? negative? neg :when? (> _ 20) (return 11) 1)
-(|> 5 (+ _ 7) double :(prn "hello") (+ _ 3) (return 99) neg :when? negative? neg :when? (> _ 20) (return 11) 1)
+(|> 5 (+ _ 7) double :(prn "hello") (+ _ 3) neg :when negative? neg :when (> _ 20) (return 11) 1)
+(|> 5 (+ _ 7) :(prn  "hello") (+ _ 3) (return 99) neg :when negative? neg :when (> _ 20) (return 11) 1)
+(|> 5 (+ _ 7) double :(prn "hello") (+ _ 3) (return 99) neg :when negative? neg :when (> _ 20) (return 11) 1)
 
 ;;;;;;;;;;;
 (|> ((x)) 5 (+ x 7) double :(prn "hello") (+ x 3) neg :?(when (negative? x) (neg x)) :(when (> x 20) (return 11)) 1)
 (|> ((x)) 5 (+ x 7) :(prn "hello") (+ x 3) (return 99) neg :?(when (negative? x) (neg x)) :(when (> x 20) (return 11)) 1)
 (|> ((x)) 5 (+ x 7) double :(prn "hello") (+ x 3) (return 99) neg :?(when (negative? x) (neg x)) :(when (> x 20) (return 11)) 1)
 
-(|> ((x)) 5 (+ x 7) double :(prn "hello") (+ x 3) neg :when? negative? neg :when? (> x 20) (return 11) 1)
-(|> ((x)) 5 (+ x 7) :(prn "hello") (+ x 3) (return 99) neg :when? negative? neg :when? (> x 20) (return 11) 1)
-(|> ((x)) 5 (+ x 7) double :(prn "hello") (+ x 3) (return 99) neg :when? negative? neg :when? (> x 20) (return 11) 1)
+(|> ((x)) 5 (+ x 7) double :(prn "hello") (+ x 3) neg :when negative? neg :when (> x 20) (return 11) 1)
+(|> ((x)) 5 (+ x 7) :(prn "hello") (+ x 3) (return 99) neg :when negative? neg :when (> x 20) (return 11) 1)
+(|> ((x)) 5 (+ x 7) double :(prn "hello") (+ x 3) (return 99) neg :when negative? neg :when (> x 20) (return 11) 1)
 
 ;;;;;;;;;;;
 (|> ((x 5)) (+ x 7) double :(prn "hello") (+ x 3) neg :?(when (negative? x) (neg x)) :(when (> x 20) (return 11)) 1)
 (|> ((x 5)) (+ x 7) :(prn "hello") (+ x 3) (return 99) neg :?(when (negative? x) (neg x)) :(when (> x 20) (return 11)) 1)
 (|> ((x 5)) (+ x 7) double :(prn "hello") (+ x 3) (return 99) neg :?(when (negative? x) (neg x)) :(when (> x 20) (return 11)) 1)
 
-(|> ((x 5)) (+ x 7) double :(prn "hello") (+ x 3) neg :when? negative? neg :when? (> x 20) (return 11) 1)
-(|> ((x 5)) (+ x 7) :(prn "hello") (+ x 3) (return 99) neg :when? negative? neg :when? (> x 20) (return 11) 1)
-(|> ((x 5)) (+ x 7) double :(prn "hello") (+ x 3) (return 99) neg :when? negative? neg :when? (> x 20) (return 11) 1)
+(|> ((x 5)) (+ x 7) double :(prn "hello") (+ x 3) neg :when negative? neg :when (> x 20) (return 11) 1)
+(|> ((x 5)) (+ x 7) :(prn "hello") (+ x 3) (return 99) neg :when negative? neg :when (> x 20) (return 11) 1)
+(|> ((x 5)) (+ x 7) double :(prn "hello") (+ x 3) (return 99) neg :when negative? neg :when (> x 20) (return 11) 1)
 
 ;;;;;;;;;;;
-(|> 5 :when? odd? :return 100)
-(|> 6 :when? odd? :return 100)
+(|> 5 :when odd? :return 100)
+(|> 6 :when odd? :return 100)
 (|> 5 :return 100)
 (|> 6 :return 100)
+(|> 5 :when odd? :? 100)
+(|> 6 :when odd? :? 100)
+(|> 5 :when odd? :maybe 100)
+(|> 6 :when odd? :maybe 100)
