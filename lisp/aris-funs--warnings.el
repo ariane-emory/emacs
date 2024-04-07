c80ec38339071801bb0f0198465c3332eb3ad22b
Ariane Emory <ariane.emory@protonmail.com>
2024-04-07 16:09:05 -0400

    ...

---
+++ lisp/aris-funs--warnings.el
@@ -0,0 +1,26 @@
+;; -*- fill-column: 80;  eval: (display-fill-column-indicator-mode 1); eval: (variable-pitch-mode -1); -*-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Pattern matching functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro warn-unless-equal (expr val &optional msg)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Warn unless EXPR is equal to VAL"
+  `(let ((result ,expr))
+     (unless (equal result ,val)
+       (if ,msg
+         (warn "ARI'S WARNING: %s, %S != %S" ,msg ',expr ,val)
+         (warn "ARI's WARNING: %S != %S" ',expr ,val)))))
+       ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro warn-unless (expr &optional msg)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Warn unless EXPR evaluates to a non-nil value."
+  `(unless ,expr
+     (if ,msg
+       (warn "ARI's WARNING: %s, %S is nil" ',expr ,msg)
+       (warn "ARI's WARNING: %S is nil" ',expr))))
+       ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
