733ee62b3df12008e1e731850a2683a797c64f85
Ariane Emory <ariane.emory@protonmail.com>
2024-04-07 15:58:49 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -324,3 +324,20 @@
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro warn-unless-eq (expr val &optional msg)
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Warn unless EXPR is equal to VAL"
+  `(let ((result ,expr))
+     (unless (equal result ,val)
+       (if ,msg
+         (warn "ARI'S WARNING, %s: %S != %S" ,msg ,expr ,val)
+         (warn "ARI's WARNING: %S != %S" ,expr ,val)))))
+       ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(warn-unless-eq (+ 2 3) 5)
+(warn-unless-eq (+ 2 3) 7)
+
+(warn-unless-eq (+ 2 3) 5 "math is broken")
+(warn-unless-eq (+ 2 3) 7 "math is broken")
