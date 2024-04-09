6dc7913a90a9b8d58c207376517d373eaf7afde0
Ariane Emory <ariane.emory@protonmail.com>
2024-04-09 09:47:55 -0400

    ...

--- lisp/aris-funs--stacks.el
+++ lisp/aris-funs--stacks.el
@@ -101,7 +101,7 @@ followingstack operators are defined: `push!', `pop!', `swap!', `dup!', `rotl!',
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro doforth (spec &rest body)
+(defmacro doforthy (spec &rest body)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "A dumb little Forth-like stack machine without enough operations to be very useful,
 meant mainly for use in dostack's unit tests."
@@ -124,7 +124,7 @@ meant mainly for use in dostack's unit tests."
                             (throw ,return-label (or val ,val-sym)))
                           (stop! ()
                             (return! (list (out) ,val-sym (stack)))))
-               (prn "doforth: %S with %S ahead." ,val-sym (stack))
+               (prn "doforthy: %S with %S ahead." ,val-sym (stack))
                (cond 
                  ((eq? :dup    ,val-sym) (dup!))
                  ((eq? :drop   ,val-sym) (pop!))
@@ -165,49 +165,49 @@ meant mainly for use in dostack's unit tests."
   (confirm that (dostack (x '(1 2 3 4 5 6 7 8)) (when (eql? x 5) (stop!))) returns (6 7 8))
   (confirm that (dostack (x '(1 2 3 4 5 6 7 8))) returns nil)
 
-  (confirm that (doforth (_ '(:drop 3 2 1))) returns (2 1))
-  (confirm that (doforth (_ '(3 :drop 2 1))) returns (3 1))
-  (confirm that (doforth (_ '(3 2 :drop 1))) returns (3 2))
+  (confirm that (doforthy (_ '(:drop 3 2 1))) returns (2 1))
+  (confirm that (doforthy (_ '(3 :drop 2 1))) returns (3 1))
+  (confirm that (doforthy (_ '(3 2 :drop 1))) returns (3 2))
 
-  (confirm that (doforth (_ '(:dup 3 2 1))) returns (3 3 2 1))
-  (confirm that (doforth (_ '(3 :dup 2 1))) returns (3 2 2 1))
-  (confirm that (doforth (_ '(3 2 :dup 1))) returns (3 2 1 1))
+  (confirm that (doforthy (_ '(:dup 3 2 1))) returns (3 3 2 1))
+  (confirm that (doforthy (_ '(3 :dup 2 1))) returns (3 2 2 1))
+  (confirm that (doforthy (_ '(3 2 :dup 1))) returns (3 2 1 1))
 
-  (confirm that (doforth (_ '(:over 3 2 1))) returns (2 3 2 1))
-  (confirm that (doforth (_ '(:over 3 2 1 :over 5 4))) returns (2 3 2 1 4 5 4))
+  (confirm that (doforthy (_ '(:over 3 2 1))) returns (2 3 2 1))
+  (confirm that (doforthy (_ '(:over 3 2 1 :over 5 4))) returns (2 3 2 1 4 5 4))
 
-  (confirm that (doforth (_ '(:rotl 4 3 2 1))) returns (3 2 4 1))
-  (confirm that (doforth (_ '(4 :rotl 3 2 1))) returns (4 2 1 3))
+  (confirm that (doforthy (_ '(:rotl 4 3 2 1))) returns (3 2 4 1))
+  (confirm that (doforthy (_ '(4 :rotl 3 2 1))) returns (4 2 1 3))
 
-  (confirm that (doforth (_ '(:rotr 4 3 2 1))) returns (2 4 3 1))
-  (confirm that (doforth (_ '(4 :rotr 3 2 1))) returns (4 1 3 2))
+  (confirm that (doforthy (_ '(:rotr 4 3 2 1))) returns (2 4 3 1))
+  (confirm that (doforthy (_ '(4 :rotr 3 2 1))) returns (4 1 3 2))
 
-  (confirm that (doforth (_ '(:swap 3 2 1))) returns (2 3 1))
-  (confirm that (doforth (_ '(3 :swap 2 1))) returns (3 1 2))
+  (confirm that (doforthy (_ '(:swap 3 2 1))) returns (2 3 1))
+  (confirm that (doforthy (_ '(3 :swap 2 1))) returns (3 1 2))
 
   (confirm that
-    (doforth (_ '(:over 1 :rotl 2 3 4 :drop 100 5 :swap 9 :rotr 8 10 :dup twice))) 
+    (doforthy (_ '(:over 1 :rotl 2 3 4 :drop 100 5 :swap 9 :rotr 8 10 :dup twice))) 
     returns (1 3 2 4 5 10 9 8 twice twice))
 
-  (confirm that (doforth (_ '(9 :dup 8 :swap 7 :drop 6 :over 5 :rotl 4 :rotr 3 2 1))) 
+  (confirm that (doforthy (_ '(9 :dup 8 :swap 7 :drop 6 :over 5 :rotl 4 :rotr 3 2 1))) 
     returns (9 8 8 6 5 2 4 3 1))
 
-  (confirm that (doforth (x '(1 2 :swap 3 4 5 6 :drop 7 8 9)) (push-out! x))
+  (confirm that (doforthy (x '(1 2 :swap 3 4 5 6 :drop 7 8 9)) (push-out! x))
     returns (1 2 4 3 5 6 8 9))
 
-  (confirm that (doforth (x '(1 2 :swap 3 4 5 6 :drop 7 8 9))
+  (confirm that (doforthy (x '(1 2 :swap 3 4 5 6 :drop 7 8 9))
                   (when (odd? x) (push-out! x)))
     returns (1 3 5 9))
 
-  (confirm that (doforth (x '(1 2 3 :stop 4 5 6 7 8))) returns ((1 2 3) :stop (4 5 6 7 8)))
-  (confirm that (doforth (x '(1 2 3 :return 4 5 6 7 8))) returns :return)
+  (confirm that (doforthy (x '(1 2 3 :stop 4 5 6 7 8))) returns ((1 2 3) :stop (4 5 6 7 8)))
+  (confirm that (doforthy (x '(1 2 3 :return 4 5 6 7 8))) returns :return)
 
-  (confirm that (doforth (x '(1 2 :swap 3 4 5 6 :drop 7 8 9))
+  (confirm that (doforthy (x '(1 2 :swap 3 4 5 6 :drop 7 8 9))
                   (when (odd? x) (push-out! x))
                   (when (eql? 8 x) (stop!)))
     returns ((1 3 5) 8 (9)))
 
-  (confirm that (doforth (x '(1 2 :swap 3 4 5 6 :drop 7 8 9 10 11 12))
+  (confirm that (doforthy (x '(1 2 :swap 3 4 5 6 :drop 7 8 9 10 11 12))
                   (when (odd? x) (push-out! x))
                   (when (eql? 10 x) (stop!))) 
     returns ((1 3 5 9) 10 (11 12)))
