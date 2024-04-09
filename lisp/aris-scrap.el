7347a4006cfd304010f345f3a62dc67f9894bed3
Ariane Emory <ariane.emory@protonmail.com>
2024-04-08 20:44:07 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -1,4 +1,4 @@
-;; -*- lexical-binding: nil; fill-column: 90;  eval: (display-fill-column-indicator-mode 1); eval: (variable-pitch-mode -1); eval: (company-posframe-mode -1) -*-
+;; -*- lexical-binding: nil; fill-column: 90; lisp-indent-offset: 1; eval: (display-fill-column-indicator-mode 1); eval: (variable-pitch-mode -1); eval: (company-posframe-mode -1) -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'pp)
 (require 'aris-funs--alists)
@@ -16,37 +16,37 @@
 ;; MATCH2:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when nil
-  (progn
+ (progn
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    (match2 '((? . v) (* . w) 4 5 (? . x) (even? . y)) '(77 1 2 3 4 5 66 22))
+  (match2 '((? . v) (* . w) 4 5 (? . x) (even? . y)) '(77 1 2 3 4 5 66 22))
 
-    (let ( (*mp--use-new-pipe-macro* t)
-           (*mp--verbose* t)
-           (*mp--merge-duplicate-alist-keys* nil)
-           (*mp--use-dotted-pairs-in-result* nil))
-      (match2 '((* . a) 6 7 (even? . b)) '(1 2 3 4 5 6 7 8)))
-    
-    (let ( (*mp--use-new-pipe-macro* nil)
-           (*mp--verbose* t)
-           (*mp--merge-duplicate-alist-keys* nil)
-           (*mp--use-dotted-pairs-in-result* nil))
-      (match2 '((* . a) 6 7 (even? . b)) '(1 2 3 4 5 6 7 8)))
+  (let ( (*mp--use-new-pipe-macro* t)
+        (*mp--verbose* t)
+        (*mp--merge-duplicate-alist-keys* nil)
+        (*mp--use-dotted-pairs-in-result* nil))
+   (match2 '((* . a) 6 7 (even? . b)) '(1 2 3 4 5 6 7 8)))
+  
+  (let ( (*mp--use-new-pipe-macro* nil)
+        (*mp--verbose* t)
+        (*mp--merge-duplicate-alist-keys* nil)
+        (*mp--use-dotted-pairs-in-result* nil))
+   (match2 '((* . a) 6 7 (even? . b)) '(1 2 3 4 5 6 7 8)))
 
-    (let ( (*mp--use-new-pipe-macro* t)
-           (*mp--verbose* t)
-           (*mp--merge-duplicate-alist-keys* t)
-           (*mp--use-dotted-pairs-in-result* nil))
-      (match2 '((* . a) 6 7 (even? . b)) '(1 2 3 4 5 6 7 8)))
-    
-    (let ( (*mp--use-new-pipe-macro* nil)
-           (*mp--verbose* t)
-           (*mp--merge-duplicate-alist-keys* t)
-           (*mp--use-dotted-pairs-in-result* nil))
-      (match2 '((* . a) 6 7 (even? . b)) '(1 2 3 4 5 6 7 8)))
-    
-    (merge-duplicate-alist-keys '((a 1) (a 2) (a 3) (a 4) (a 5) (b 8)))
+  (let ( (*mp--use-new-pipe-macro* t)
+        (*mp--verbose* t)
+        (*mp--merge-duplicate-alist-keys* t)
+        (*mp--use-dotted-pairs-in-result* nil))
+   (match2 '((* . a) 6 7 (even? . b)) '(1 2 3 4 5 6 7 8)))
+  
+  (let ( (*mp--use-new-pipe-macro* nil)
+        (*mp--verbose* t)
+        (*mp--merge-duplicate-alist-keys* t)
+        (*mp--use-dotted-pairs-in-result* nil))
+   (match2 '((* . a) 6 7 (even? . b)) '(1 2 3 4 5 6 7 8)))
+  
+  (merge-duplicate-alist-keys '((a 1) (a 2) (a 3) (a 4) (a 5) (b 8)))
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-    ))
+  ))
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
@@ -54,41 +54,41 @@
 ;;PATTERN-DISPATCH and some PIPE:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when nil
-  (progn
-    (let ((*wm--depth-indicator-enable*))
+ (progn
+  (let ((*wm--depth-indicator-enable*))
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-      ;; Reset the pattern-call dispatcher's alist:
-      (pd--reset)
+   ;; Reset the pattern-call dispatcher's alist:
+   (pd--reset)
 
-      ;; Define some simple functions:
-      (def (doub n) (|> n (+ _ _)))
-      (def (sqr y) (|> y (* _ _)))
-      (def (doub-sqr y) (doub (sqr y)))
+   ;; Define some simple functions:
+   (def (doub n) (|> n (+ _ _)))
+   (def (sqr y) (|> y (* _ _)))
+   (def (doub-sqr y) (doub (sqr y)))
 
-      ;; Define a fib:
-      (def (fib 0) 0)
-      (def (fib 1) 1)
-      (def (fib n)
-        (|>
-          :(prn "Calculating (fib %d) using a pipe-based fib..." n)
-          (|> n
-            (- _ 1)
-            (fib _))
-          (+ _
-            (|> n
-              (- _ 2)
-              (fib _)
-              :(prn "Calculated (fib %d) = %d" n _)))))
+   ;; Define a fib:
+   (def (fib 0) 0)
+   (def (fib 1) 1)
+   (def (fib n)
+    (|>
+     :(prn "Calculating (fib %d) using a pipe-based fib..." n)
+     (|> n
+      (- _ 1)
+      (fib _))
+     (+ _
+      (|> n
+       (- _ 2)
+       (fib _)
+       :(prn "Calculated (fib %d) = %d" n _)))))
 
-      ;; Call it with some output commenting on the proceedings:
-      (|> 3
-        :(prn "Starting out with %d" _)
-        (+ _ (|> 2 (+ _ 5)))
-        :(prn "Getting the result of (fib %d)" _)
-        (fib _)
-        :(prn "Result =  %d" _)) ;; ⇒ 55
+   ;; Call it with some output commenting on the proceedings:
+   (|> 3
+    :(prn "Starting out with %d" _)
+    (+ _ (|> 2 (+ _ 5)))
+    :(prn "Getting the result of (fib %d)" _)
+    (fib _)
+    :(prn "Result =  %d" _)) ;; ⇒ 55
         ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-      )))
+   )))
       ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
@@ -114,82 +114,78 @@
 ;;   out)
 
 (when nil
-  (dostack (x body)
-    (prn "Item:      %S" x)
-    (prn "Remaining: %S" stack))
+ (dostack (x body)
+  (prn "Item:      %S" x)
+  (prn "Remaining: %S" stack))
 
-  (let (out)
-    (dostack (x '(1 2 3 4 5 6 7 8 9))
-      (prn "Item:      %S" x)
-      (prn "Remaining: %S" stack)
-      (when (eql x 4) (push! 22))
-      (when (eql x 7) (return! 99))
-      (setq out (cons x out))
-      )
-    out))
+ (let (out)
+  (dostack (x '(1 2 3 4 5 6 7 8 9))
+   (prn "Item:      %S" x)
+   (prn "Remaining: %S" stack)
+   (when (eql x 4) (push! 22))
+   (when (eql x 7) (return! 99))
+   (setq out (cons x out))
+   )
+  out))
 
 (defun --dosk-validate-spec (spec)
-  (unless (cons? spec)
-    (signal 'wrong-type-argument (list 'cons? spec)))
-  (unless (= 2 (length spec))
-    (signal 'wrong-number-of-arguments (list '(2 . 2) (length spec)))))
+ (unless (cons? spec)
+  (signal 'wrong-type-argument (list 'cons? spec)))
+ (unless (= 2 (length spec))
+  (signal 'wrong-number-of-arguments (list '(2 . 2) (length spec)))))
 
 (defmacro dostk (spec &rest body)
-  (--dosk-validate-spec spec)
-  (let* ( (val-sym (car spec))
-          (stack (nth 1 spec))
-          ;;(stack-sym (gensym "stack-"))
-          (stack-sym (if (symbolp stack) stack (gensym "stack-")))
-          )
-    `(let ( (stack-is-sym ,(symbolp stack))
-            (,stack-sym ,stack))
-       (cl-labels ( (pop! ()
-                      (pop ,stack-sym)))
-         (while ,stack-sym
-           (let ( (,val-sym (pop!))
-                  (stack ,stack-sym))
-             ,@body))))))
-
+ (--dosk-validate-spec spec)
+ (let* ( (val-sym (car spec))
+        (stack (nth 1 spec))
+        (stack-is-sym (symbolp stack))
+        (stack-sym (if stack-is-sym stack (gensym "stack-")))
+        (stack-let-binding (unless stack-is-sym
+                            (list (list stack-sym stack))))
+        (return-label `',(gensym "return-"))
+        )
+  `(catch ,return-label
+    (let ( (stack-is-sym ,stack-is-sym)
+          ,@stack-let-binding)
+     (cl-labels ( (pop! () (pop ,stack-sym)) )
+      (while ,stack-sym
+       (let ((,val-sym (pop!)))
+        ,@body))
+      ;; Return whatever part of the stack remains;
+      ,stack-sym)))))
 
 (setq stk '(1 2 3 4 5 6 7 8))
 
 (dostk (x stk)
-  (prn "Val: %S" x))
+ (prn "Val: %S" x))
 
 (let
-  ((stack-is-sym t)
-    (stk stk))
-  (cl-labels
-    ((pop! nil
-       (pop stk)))
-    (while stk
-      (let
-        ((x
-           (pop!))
-          (stack stk))
-        (prn "Val: %S" x)))))
+ ((stack-is-sym t))
+ (cl-labels
+  ((pop! nil
+    (pop stk)))
+  (while stk
+   (let
+    ((x
+      (pop!)))
+    (prn "Val: %S" x)))))
 
 (dostk (x '(1 2 3 4 5 6 7 8))
-  (prn "Val: %S" x))
+ (prn "Val: %S" x))
+
 (let
-  ((stack-is-sym nil)
-    (stack-1050
-      '(1 2 3 4 5 6 7 8)))
-  (cl-labels
-    ((pop! nil
-       (pop stack-1050)))
-    (while stack-1050
-      (let
-        ((x
-           (pop!))
-          (stack stack-1050))
-        (prn "Val: %S" x)))))
-
-
-
-
-
-
-
+ ((stack-is-sym nil)
+  (stack-1213
+   '(1 2 3 4 5 6 7 8)))
+ (cl-labels
+  ((pop! nil
+    (pop stack-1213)))
+  (while stack-1213
+   (let
+    ((x
+      (pop!)))
+    (prn "Val: %S" x)))
+  stack-1213
+  ))
 
 
