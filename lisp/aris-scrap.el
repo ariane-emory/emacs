121ad033d2de1ddb558d3cc082192316b69b0a3e
Ariane Emory <ariane.emory@protonmail.com>
2024-04-10 18:27:11 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -271,6 +271,52 @@
          ,then
          ,@else))))
 
+
+
+(cl-defmacro when-let (bindings &body body)
+  "Bind `bindings` in parallel and execute `body`, short-circuiting on `nil`.
+See: https://stevelosh.com/blog/2018/07/fun-with-macros-if-let/ "
+  (let ((block (gensym "block-")))
+    `(cl-block ,block
+       (let* ,(cl-loop for (symbol value) in bindings
+                collect `(,symbol (or ,value (cl-return-from ,block nil))))
+         ,@body))))
+
+(cl-defmacro if-let (bindings then &body else)
+  "Bind `bindings` in parrallel and execute `then` if all are true, or `else` otherwise.
+See: https://stevelosh.com/blog/2018/07/fun-with-macros-if-let/"
+  (let ( (outer (gensym "outer-"))
+         (inner (gensym "inner-")))
+    `(cl-block ,outer
+       (cl-block ,inner
+         (let ,(cl-loop for (symbol value) in bindings
+                 collect `(,symbol (or ,value (cl-return-from ,inner nil))))
+           (cl-return-from ,outer ,then)))
+       ,@else)))
+
+(cl-defmacro when-let* (bindings &body body)
+  "Bind `bindings` serially and execute `body`, short-circuiting on `nil`.
+See: https://stevelosh.com/blog/2018/07/fun-with-macros-if-let/ "
+  (let ((block (gensym "block-")))
+    `(cl-block ,block
+       (let* ,(cl-loop for (symbol value) in bindings
+                collect `(,symbol (or ,value (cl-return-from ,block nil))))
+         ,@body))))
+
+(cl-defmacro if-let* (bindings then &body else)
+  "Bind `bindings` serially and execute `then` if all are true, or `else` otherwise.
+
+See: https://stevelosh.com/blog/2018/07/fun-with-macros-if-let/"
+  (let ( (outer (gensym "outer-"))
+         (inner (gensym "inner-")))
+    `(cl-block ,outer
+       (cl-block ,inner
+         (let* ,(cl-loop for (symbol value) in bindings
+                  collect `(,symbol (or ,value (cl-return-from ,inner nil))))
+           (cl-return-from ,outer ,then)))
+       ,@else)))
+
+
 (when-let ((a 1))
   (list a))
 
@@ -278,87 +324,3 @@
   (* a a)
   (prn "extra")
   100)
-
-
-(cl-defmacro when-let* (bindings &body body)
-  "Bind `bindings` serially and execute `body`, short-circuiting on `nil`.
-
-  This macro combines `when` and `let*`.  It takes a list of bindings
-  and binds them like `let*` before executing `body`, but if any
-  binding's value evaluates to `nil` the process stops and `nil` is
-  immediately returned.
-
-  Examples:
-
-    (when-let* ((a (progn (print :a) 1))
-                (b (progn (print :b) (1+ a)))
-      (list a b)))
-    ; =>
-    :A
-    :B
-    (1 2)
-
-    (when-let* ((a (progn (print :a) nil))
-                (b (progn (print :b) (1+ a))))
-      (list a b))
-    ; =>
-    :A
-    NIL
-
-  "
-  (let ((block (gensym "block-")))
-    `(cl-block ,block
-       (let* ,(cl-loop for (symbol value) in bindings
-                collect `(,symbol (or ,value
-                                    (cl-return-from ,block nil))))
-         ,@body))))
-
-
-(when-let* ( (x 1)
-             (y 2)
-             (z (print "foo")))
-  (list x y z))
-
-
-(defmacro if-let* (bindings then &rest else)
-  "Bind `bindings` serially and execute `then` if all are true, or `else` otherwise.
-
-  This macro combines `if` and `let*`.  It takes a list of bindings and
-  binds them like `let*` before executing `then`, but if any binding's
-  value evaluates to `nil` the process stops and the `else` branch is
-  immediately executed (with no bindings in effect).
-
-  Examples:
-
-    (if-let* ((a (progn (print :a) 1))
-              (b (progn (print :b) (1+ a)))
-      (list a b)
-      'nope)
-    ; =>
-    :A
-    :B
-    (1 2)
-
-    (if-let* ((a (progn (print :a) nil))
-              (b (progn (print :b) (1+ a))))
-      (list a b)
-      'nope)
-    ; =>
-    :A
-    NOPE
-
-  "
-  (let ( (outer (gensym "outer-"))
-         (inner (gensym "inner-")))
-    `(cl-block ,outer
-       (cl-block ,inner
-         (let* ,(cl-loop for (symbol value) in bindings
-                  collect `(,symbol (or ,value
-                                      (cl-return-from ,inner nil))))
-           (cl-return-from ,outer ,then)))
-       ,@else)))
-
-(if-let* ( (a (progn (print :a) nil))
-           (b (progn (print :b) (1+ a))))
-  (list a b)
-  'nope)
