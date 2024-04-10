d2ae637fd2ff14110e69d10cfe14664a4dfedf82
Ariane Emory <ariane.emory@protonmail.com>
2024-04-10 16:57:11 -0400

    ...

--- lisp/aris-funs--warnings.el
+++ lisp/aris-funs--warnings.el
@@ -5,25 +5,34 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defcustom *warnings-enabled* t
+  "Whether to use my warning functions."
+  :type 'boolean)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defmacro warn-unless-equal (expr val &optional msg)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Warn unless EXPR evaluates to VAL"
-  `(let ((result ,expr))
-     (unless (equal result ,val)
-       (if ,msg
-         (warn "ARI'S WARNING: %s, %S != %S" ,msg ',expr ,val)
-         (warn "ARI's WARNING: %S != %S" ',expr ,val)))))
-         ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  `(when *warnings-enabled*
+     (let ((result ,expr))
+       (unless (equal result ,val)
+         (if ,msg
+           (warn "ARI'S WARNING: %s, %S != %S" ,msg ',expr ,val)
+           (warn "ARI's WARNING: %S != %S" ',expr ,val))))))
+           ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defmacro warn-unless (expr &optional msg)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Warn unless EXPR evaluates to a non-nil value."
-  `(unless ,expr
-     (if ,msg
-       (warn "ARI's WARNING: %s, %S is nil" ',expr ,msg)
-       (warn "ARI's WARNING: %S is nil" ',expr))))
+  `(when *warnings-enabled*
+     (unless ,expr
+       (if ,msg
+         (warn "ARI's WARNING: %s, %S is nil" ',expr ,msg)
+         (warn "ARI's WARNING: %S is nil" ',expr)))))
        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
