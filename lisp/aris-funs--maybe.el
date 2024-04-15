70a1181e4ee2877ce5543d685f5af84fdb0d6d67
Ariane Emory <ariane.emory@protonmail.com>
2024-04-14 20:47:46 -0400

    ...

--- lisp/aris-funs--maybe.el
+++ lisp/aris-funs--maybe.el
@@ -1,15 +1,34 @@
 ;; -*- fill-column: 90;  eval: (display-fill-column-indicator-mode 1); eval: (variable-pitch-mode -1); -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; maybe macro:
+s;; maybe macro:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 (defmacro maybe (type val)
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Return VAL when it is of type TYPE, otherwise return nil."
   `(and
      (cl-typep ,val
        ,(if (and (symbolp type) (get type 'cl-deftype-satisfies)) `',type type))
      ,val))
-     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro maybe (type val)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Return VAL when it is of type TYPE, otherwise return nil."
+  `(and (cl-typep ,val ',type) ,val))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defmacro maybe (type val)
+  "Return VAL when it is of type TYPE, otherwise return nil."
+  (let ((type-form
+          (cond ((eq (car-safe type) 'quote) type)
+            ((symbolp type) `(quote ,type))
+            (t type))))
+    `(when (cl-typep ,val ,type-form)
+       ,val)))
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
