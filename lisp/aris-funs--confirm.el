98060d4f1594f7200c5f0d220cd3cea459afb3a2
Ariane Emory <ariane.emory@protonmail.com>
2024-04-17 14:47:12 -0400

    ...

--- lisp/aris-funs--confirm.el
+++ lisp/aris-funs--confirm.el
@@ -10,7 +10,7 @@
 (defmacro confirm (that expr returns expected)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Test whether EXPR evaluates to EXPECTED."
-  (unless (eq? 'that   that)     (error "expected 'that as 1st argument"))
+  (unless (eq? 'that    that)    (error "expected 'that as 1st argument"))
   (unless (eq? 'returns returns) (error "expected 'returns as 3rd argument"))
   `(let ((result ,expr))
      (unless (equal result ',expected)
