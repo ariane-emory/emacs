9852baae616fdb3b8508742f8e0ad49530ba6d01
Ariane Emory <ariane.emory@protonmail.com>
2024-04-17 13:33:20 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -85,3 +85,12 @@ marked pure mainly to test if DECLARE-FORM is handled properly."
 
 ;; breaking case, genuinely malformed:
 (|> 1 :unless t :unless T)
+
+
+
+;; naming examples for an imaginary frobnosticate-widget package:
+fw--frobnosticate-widget    ;; a public-facing function in the frobnosticate-widget package.
+--fw-frobnostication-helper ;; an internal function in the frobnosticate-widget package.
+*fw--frobnostication-level* ;; a public-facing (usually customizable) variable in the frobnosticate-widget package.
+*--fw-frobnostication-mode* ;; an internal variable in the frobnosticate-widget package not meant for customization
+frobnosticate-widget        ;; an convenient alias for fw--frobnosticate-widget.
