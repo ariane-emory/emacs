ac3e4df17b1707871cb96c2aa0827263706dc932
Ariane Emory <ariane.emory@protonmail.com>
2024-04-17 14:02:50 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -20,7 +20,8 @@
 ;; --do-fw-stuff                ;; an internal function in the frobnosticate-widget package.
 ;; *fw--frobnostication-level*  ;; a public-facing (usually customizable) variable in the frobnosticate-widget package.
 ;; *--fw-frobnostication-count* ;; an internal variable in the frobnosticate-widget package not meant for customization
-;; frobnosticate-widget         ;; an convenient alias for fw--frobnosticate-widget.
+;; frobnosticate-widget         ;; either a public-facing function in the frobnosticate-widget package or a convenient
+;;                              ;;  alias for fw--frobnosticate-widget.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
