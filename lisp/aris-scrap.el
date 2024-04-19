00be1b718cde1c3b5e2b6635f76ea245e5361320
Ariane Emory <ariane.emory@protonmail.com>
2024-04-19 15:26:34 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -335,7 +335,12 @@ marked pure mainly to test if DECLARE-FORM is handled properly."
         (loop b (+ a b) (- i 1))))))
 
 (def (fib (n : positive-integer)) ; => positive-integer
-  (|> `(a 0 b 1 i ,n) 'loop :when (zero? (plist-get _ 'i)) :go 'exit 'exit (plist-get _ 'a))
+  (|>
+    `(a 0 b 1 i ,n)
+    'loop
+    :when (zero? (plist-get _ 'i)) :go 'exit
+    'exit
+    (plist-get _ 'a))
 
   )
 (fib 10)
