fd53e3ca366793748bd0c533a54a496d8921f2ab
Ariane Emory <ariane.emory@protonmail.com>
2024-04-20 16:50:25 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -50,58 +50,14 @@
   (* n n))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun-memo bar (x y) (prn "Calculate %s x %s.." x y ) (* x y))
 
 (bar 7 8)
 (bar 7 8)
 (bar 7 9)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (symbol-function 'bar)
-(lambda
-  (&rest args)
-  (let ((val (gethash args #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()) 'memo-not-found-898)))
-    (if (eq val 'memo-not-found-898)
-      (setf (gethash args #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()))
-        (apply
-          (lambda (x y)
-            (prn "Calculate %s x %s.." x y)
-            (* x y))
-          args))
-      val)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(lambda (&rest args)
-  (let ((val (gethash args #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()) 'memo-not-found-896)))
-    (if (eq val 'memo-not-found-896)
-      (setf (gethash args #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()))
-        (apply
-          (lambda
-            (x y)
-            (prn "Calculate %s x %s.." x y)
-            (* x y))
-          args))
-      val)))
-
-(lambda (&rest args)
-  (let ((val (gethash args #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()) 'memo-not-found-895)))
-    (if (eq val 'memo-not-found-895)
-      (setf (gethash args #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()))
-        (apply
-          (lambda (x y)
-            (prn "Calculate %s x %s.." x y)
-            (* x y))
-          args))
-      val)))
-
-(lambda (&rest args)
-  (let* ( (k args)
-          (val (gethash k #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()) 'memo-not-found-893)))
-    (if (eq val 'memo-not-found-893)
-      (setf (gethash k #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()))
-        (apply
-          (lambda (x y)
-            (prn "Calculate %s x %s.." x y)
-            (* x y))
-          args))
-      val)))
