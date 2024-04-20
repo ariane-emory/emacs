d1911e45a63729a3a79729a6a6eb2ab3d6c569df
Ariane Emory <ariane.emory@protonmail.com>
2024-04-19 22:20:47 -0400

    ...

--- lisp/aris-naming-conventions.el
+++ lisp/aris-naming-conventions.el
@@ -1,12 +1,24 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; naming examples for an imaginary frobnosticate-widget package:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; fw--frobnosticate-widget     ;; a public-facing function in the frobnosticate-widget package.
-;; --fw-frobnostication-helper  ;; an internal function in the frobnosticate-widget package.
-;; --do-fw-stuff                ;; an internal function in the frobnosticate-widget package.
-;; *fw--frobnostication-level*  ;; a public-facing (usually customizable) variable in the frobnosticate-widget package.
-;; *--fw-frobnostication-count* ;; an internal variable in the frobnosticate-widget package not meant for customization
-;; frobnosticate-widget         ;; either a public-facing function in the frobnosticate-widget package or a convenient
-;;                              ;;  alias for fw--frobnosticate-widget.
+(let ( (aris-sample-names 
+         '( ('fw--frobnosticate-widget .
+              "a public-facing function in the frobnosticate-widget package.")
+            ('--fw-frobnostication-helper .
+              "an internal function in the frobnosticate-widget package.")
+            ('--do-fw-stuff .
+              "an internal function in the frobnosticate-widget package.")
+            ('*fw--frobnostication-level* .
+              "a public-facing (usually customizable) variable in the frobnosticate-widget package.")
+            ('*--fw-frobnostication-count* .
+              "an internal variable in the frobnosticate-widget package not meant for customization")
+            ('frobnosticate-widget .
+              "either a public-facing function in the frobnosticate-widget package or a convenient
+alias for fw--frobnosticate-widget."))))
+  aris-sample-names)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(provide 'aris-naming-conventions)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
