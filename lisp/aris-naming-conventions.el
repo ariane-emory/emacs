5f29eb27011a7d1929d241da314f6974d000068a
Ariane Emory <ariane.emory@protonmail.com>
2024-04-19 22:24:02 -0400

    ...

--- lisp/aris-naming-conventions.el
+++ lisp/aris-naming-conventions.el
@@ -1,24 +1,25 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; -*- lexical-binding: nil; fill-column: 120; eval: (display-fill-column-indicator-mode 1);  -*-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; naming examples for an imaginary frobnosticate-widget package:
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(let ( (aris-sample-names 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(let ( (aris-naming-conventions--examples 
          '( ('fw--frobnosticate-widget .
-              "a public-facing function in the frobnosticate-widget package.")
+              "The name of a public-facing function in the frobnosticate-widget package.")
             ('--fw-frobnostication-helper .
-              "an internal function in the frobnosticate-widget package.")
+              "The name of an internal function in the frobnosticate-widget package.")
             ('--do-fw-stuff .
-              "an internal function in the frobnosticate-widget package.")
+              "The name of an internal function in the frobnosticate-widget package.")
             ('*fw--frobnostication-level* .
-              "a public-facing (usually customizable) variable in the frobnosticate-widget package.")
+              "The name of a public-facing (usually customizable) variable in the frobnosticate-widget package.")
             ('*--fw-frobnostication-count* .
-              "an internal variable in the frobnosticate-widget package not meant for customization")
+              "The name of an internal variable in the frobnosticate-widget package not meant for customization")
             ('frobnosticate-widget .
-              "either a public-facing function in the frobnosticate-widget package or a convenient
+              "The name of either a public-facing function in the frobnosticate-widget package or a convenient
 alias for fw--frobnosticate-widget."))))
   aris-sample-names)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'aris-naming-conventions)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
