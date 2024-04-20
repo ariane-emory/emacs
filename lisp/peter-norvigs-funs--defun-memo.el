73c5bed7a0b47a4360c87e990cbf2d35256cb81d
Ariane Emory <ariane.emory@protonmail.com>
2024-04-20 13:06:29 -0400

    ...

--- lisp/peter-norvigs-funs--defun-memo.el
+++ lisp/peter-norvigs-funs--defun-memo.el
@@ -14,14 +14,21 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(cl-defun memo  (fun &key (key #'first) (test #'eql) name)
+(cl-defun make-memo-fun (fun &key (key #'identity) (test #'equal) name)
+  ;; are renamed this fun from `memo' to `make-memo-fun'.
+  ;; ari changed the default key to #'first to #'identity.
+  ;; ari changed the default test to from #'eql to #'equal.
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Return a memo-function of FUN."
   (let ((table (make-hash-table :test test)))
     (setf (get name 'memo) table)
     `(lambda (&rest args)
+       (prn "args is %s" args)
        (let* ( (k (funcall #',key args))
                (val (gethash k ,table :NOT-FOUND)))
+         (prn "k is %s" k)
+         (prn "table is %s" ,table)
+         (prn "val is %s" val)
          (if (eq val :NOT-FOUND)
            (setf (gethash k ,table) (apply ,fun args))
            val)))))
@@ -29,12 +36,14 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(cl-defun memoize (fun-name &key (key #'first) (test #'eql))
+(cl-defun memoize (fun-name &key (key #'identity) (test #'equal))
+  ;; ari changed the default key to #'first to #'identity.
+  ;; ari changed the default test to from #'eql to #'equal.
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Replace fun-name's global definition with a memoized version."
   (clear-memoize fun-name)
   (setf (symbol-function fun-name)
-    (memo  (symbol-function fun-name)
+    (make-memo-fun (symbol-function fun-name)
       :name fun-name :key key :test test)))
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
