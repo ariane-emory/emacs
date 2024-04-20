2a8326d1e20d573e73dd49ce392eb17fbf4f5123
Ariane Emory <ariane.emory@protonmail.com>
2024-04-19 22:15:06 -0400

    ...

---
+++ lisp/naming.el
@@ -0,0 +1,12 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; naming examples for an imaginary frobnosticate-widget package:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; fw--frobnosticate-widget     ;; a public-facing function in the frobnosticate-widget package.
+;; --fw-frobnostication-helper  ;; an internal function in the frobnosticate-widget package.
+;; --do-fw-stuff                ;; an internal function in the frobnosticate-widget package.
+;; *fw--frobnostication-level*  ;; a public-facing (usually customizable) variable in the frobnosticate-widget package.
+;; *--fw-frobnostication-count* ;; an internal variable in the frobnosticate-widget package not meant for customization
+;; frobnosticate-widget         ;; either a public-facing function in the frobnosticate-widget package or a convenient
+;;                              ;;  alias for fw--frobnosticate-widget.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
