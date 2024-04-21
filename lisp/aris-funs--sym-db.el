d1dd60e10dab711b7926ce7820594d0b63867930
Ariane Emory <ariane.emory@protonmail.com>
2024-04-21 07:14:19 -0400

    ...

--- lisp/aris-funs--sym-db.el
+++ lisp/aris-funs--sym-db.el
@@ -6,22 +6,25 @@
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Create a new DB in the sym NAME's DB-PROP property."
   (prndiv)
-  (let* ((arglist arglist) (pos arglist) optionals)
+  (let* ( (arglist (cons :DUMMY arglist))
+          (pos arglist)
+          optionals)
     (while pos
       (prn "pos %s" pos)
-      (when (eq (first pos) '&optional)
-        (setq optionals (cdr pos))
+      (when (eq (second pos) '&optional)
+        (setq optionals (cddr pos))
         (setcdr pos nil))
       (pop pos))
-    (prn "arglist %s" arglist)
-    (prn "optionals %s" optionals)
-    (let* ( (arglist (append '(db-sym) arglist '(dp-prop) optionals)))
+    (pop arglist) ;; pop :DUMMY
+    (prn "arglist is %s" arglist)
+    (prn "optionals is %s" optionals)
+    (let* ( (arglist (append '(db-sym) arglist '(&optional db-prop) optionals)))
       (prn "arglist is now %s" arglist)
       `(defun ,name ,arglist
          (let* ( (db-prop (or db-prop 'db))
                  (db (get db-sym db-prop)))
            ,@body)))))
-       ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+           ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -29,51 +32,61 @@
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (let* ( (test-fun (or test-fun #'equal))
           (new-table (make-hash-table :test test-fun)))
-    (setf db new-table)))
+    (put db-sym db-prop new-table)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (confirm that (hash-table-p (create-db 'foo #'eq 'db)) returns t)
-;; (confirm that (hash-table-p (create-db 'foo #'equal 'alt)) returns t)
-;; (confirm that (hash-table-p (get 'foo 'db)) returns t)
-;; (confirm that (hash-table-p (get 'foo 'alt)) returns t)
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(confirm that (hash-table-p (create-db 'foo 'db #'eq)) returns t)
+(confirm that (hash-table-p (create-db 'foo 'alt)) returns t)
+(confirm that (hash-table-p (get 'foo 'db)) returns t)
+(confirm that (hash-table-p (get 'foo 'alt)) returns t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (defun--db-fun db-get (key)
-;;   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;   (with-gensyms (db-not-found)
-;;     (let* ( (got (gethash key db db-not-found))
-;;             (found (not (eq got db-not-found)))
-;;             (val (when found got)))
-;;       (cons val found))))
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (confirm that (db-get 'foo 'bar) returns (nil))
-;; (confirm that (db-get 'foo 'bar 'alt) returns (nil))
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun--db-fun db-get (key)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (with-gensyms (db-not-found)
+    (let* ( (got (gethash key db db-not-found))
+            (found (not (eq got db-not-found)))
+            (val (when found got)))
+      (cons val found))))
+(defun db-get (db-sym key &optional db-prop)
+  (let* ((db-prop (or db-prop 'db))
+          (db (get db-sym db-prop)))
+    (with-gensyms (db-not-found)
+      (let* ((got (gethash key db db-not-found))
+              (found (not (eq got db-not-found)))
+              (val (when found got)))
+        (cons val found)))))
 
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (defun--db-fun db-put (key val)
-;;   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;   (setf (gethash key db) val))
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (confirm that (db-put 'foo 'bar 777) returns 777)
-;; (confirm that (db-put 'foo 'bar 888 'alt) returns 888)
-;; (confirm that (db-get 'foo 'bar) returns (777 . t))
-;; (confirm that (db-get 'foo 'bar 'alt) returns (888 . t))
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(confirm that (db-get 'foo 'bar) returns (nil))
+(confirm that (db-get 'foo 'bar 'alt) returns (nil))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (defun--db-fun db-clear ()
-;;   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;   (clrhash db))
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (db-clear 'foo)
-;; (db-clear 'foo 'alt)
-;; (confirm that (db-get 'foo 'bar) returns (nil))
-;; (confirm that (db-get 'foo 'bar 'alt) returns (nil))
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun--db-fun db-put (key val)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (setf (gethash key db) val))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(confirm that (db-put 'foo 'bar 777) returns 777)
+(confirm that (db-put 'foo 'bar 888 'alt) returns 888)
+(confirm that (db-get 'foo 'bar) returns (777 . t))
+(confirm that (db-get 'foo 'bar 'alt) returns (888 . t))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun--db-fun db-clear ()
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (clrhash db))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(db-clear 'foo)
+(db-clear 'foo 'alt)
+(confirm that (db-get 'foo 'bar) returns (nil))
+(confirm that (db-get 'foo 'bar 'alt) returns (nil))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
