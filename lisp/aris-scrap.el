3d4da857a832f6deebcab8205c0bc9f7c2d6676f
Ariane Emory <ariane.emory@protonmail.com>
2024-04-21 15:36:38 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -3,6 +3,7 @@
 (require 'aris-fib-benchmarks)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun munge-arglist (prepend-required-args prepend-optional-args arglist)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -26,19 +27,56 @@
     (prn "required:        %s" arglist)
     (prn "optionals:       %s" optionals)
     (prn "rest:            %s" rest)
-    (let ((arglist `( ,@prepend-required-args
-                      ,@arglist
-                      &optional ,@prepend-optional-args ,@optionals
-                      ,@rest)))
+    (let* ( (optionals (when (or prepend-optional-args optionals)
+                         `(&optional ,@prepend-optional-args ,@optionals)))
+            (arglist `( ,@prepend-required-args
+                        ,@arglist
+                        ,@optionals
+                        ,@rest)))
       (prn "final:           %s" arglist)
       arglist)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;; working cases:
+;; (munge-arglist '(db-sym) '(db-prop) '(x y &optional z)) ;; => (db-sym x y &optional db-prop &optional z)
+;; (munge-arglist '(db-sym) '(db-prop) '(x y)) ;; => (db-sym x y &optional db-prop)
+;; (munge-arglist '(db-sym) '(db-prop) '(&optional z)) ;; => (db-sym &optional db-prop &optional z)
+;; (munge-arglist '(db-sym) '(db-prop) '(x y &optional z &rest body)) ;; => (db-sym x y &optional db-prop &optional z &rest body)
+;; (munge-arglist '(db-sym) '(db-prop) '(x y &rest body)) ;; => (db-sym x y &optional db-prop &rest body)
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (munge-arglist nil nil '(x y &rest body)) ;; => (x y &optional &rest body)
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; working cases:
-(munge-arglist '(db-sym) '(db-prop) '(x y &optional z)) ;; => (db-sym x y &optional db-prop &optional z)
-(munge-arglist '(db-sym) '(db-prop) '(x y)) ;; => (db-sym x y &optional db-prop)
-(munge-arglist '(db-sym) '(db-prop) '(&optional z)) ;; => (db-sym &optional db-prop &optional z)
-(munge-arglist '(db-sym) '(db-prop) '(x y &optional z &rest body)) ;; => (db-sym x y &optional db-prop &optional z &rest body)
-(munge-arglist '(db-sym) '(db-prop) '(x y &rest body)) ;; => (db-sym x y &optional db-prop &rest body)
 
-(munge-arglist nil nil '(x y &rest body)) ;; => (x y &optional &rest body)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun parse-arglist (arglist)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (prndiv)
+  (prn "initial arglist: %s" arglist)
+  (let* ( (arglist (cons :DUMMY arglist))
+          (pos arglist)
+          required
+          optional
+          rest)
+    (while pos
+      (cond
+        ((eq (second pos) '&optional)
+          (setq optional (cddr pos))
+          (setcdr pos optional)) ; snip into two separate lists. 
+        ((eq (second pos) '&rest)
+          (setq rest (cddr pos)) 
+          (setcdr pos nil)))
+      (pop pos))
+    (pop arglist) ; pop :DUMMY.
+    (prndiv)
+    (prn "required:        %s" arglist)
+    (prn "optional:        %s" optional)
+    (prn "rest:            %s" rest)
+    (list required optional rest)
+    ))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(parse-arglist '(x y)) ;; (nil nil nil)
+(parse-arglist '(x y &optional z &rest rest)) ;; (nil (z) (rest))
+(parse-arglist '(x y &optional z)) ;; (nil (z) nil)
+(parse-arglist '(x y &rest rest)) ;; (nil nil (rest))
+112 11
