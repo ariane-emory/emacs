0d45739b0b039e27ab2bd87e7c9760238d612222
Ariane Emory <ariane.emory@protonmail.com>
2024-04-21 19:31:57 -0400

    use a gensym here

--- lisp/aris-funs--sym-db.el
+++ lisp/aris-funs--sym-db.el
@@ -6,88 +6,97 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro defun--db-fun (name arglist &rest body)
+(with-gensyms (foo)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Create a new DB in the sym NAME's DB-PROP property."
-  (let ( (arglist (prepend-new-args '(db-sym) '(db-prop) nil arglist))
-         (docstring (when (stringp (first body)) (list (pop body)))))
-    `(defun ,name ,arglist
-       ,@docstring
-       (let* ( (db-prop (or db-prop 'db))
-               (db (get db-sym db-prop)))
-         ,@body))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun--db-fun create-db (&optional test-fun)
+  
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Create a new hashtable on DB-SYM's DB-PROP property."
-  (let* ( (test-fun (or test-fun #'equal))
-          (new-table (make-hash-table :test test-fun)))
-    (put db-sym db-prop new-table)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that (hash-table-p (create-db 'foo 'db #'eq)) returns t)
-(confirm that (hash-table-p (create-db 'foo 'alt)) returns t)
-(confirm that (hash-table-p (get 'foo 'db)) returns t)
-(confirm that (hash-table-p (get 'foo 'alt)) returns t)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun--db-fun has-db? ()
+  (defmacro defun--db-fun (name arglist &rest body)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Return t if DB-SYM has a DB-PROP property."
-  (hash-table-p (get db-sym db-prop)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that (has-db? 'bloop) returns nil)
-(confirm that (has-db? 'foo) returns t)
-(confirm that (has-db? 'foo 'alt) returns t)
-(confirm that (has-db? 'foo 'nope) returns nil)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    "Create a new DB in the sym NAME's DB-PROP property."
+    (let ( (arglist (prepend-new-args '(db-sym) '(db-prop) nil arglist))
+           (docstring (when (stringp (first body)) (list (pop body)))))
+      `(defun ,name ,arglist
+         ,@docstring
+         (let* ( (db-prop (or db-prop 'db))
+                 (db (get db-sym db-prop)))
+           ,@body))))
+           ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun--db-fun db-get (key)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Get the value of KEY in the hashtable on DB-SYM's DB-PROP property."
-  (with-gensyms (db-not-found)
-    (let* ( (got (gethash key db db-not-found))
-            (found (not (eq got db-not-found)))
-            (val (when found got)))
-      (cons val found))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that (db-get 'foo 'bar) returns (nil))
-(confirm that (db-get 'foo 'bar 'alt) returns (nil))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun--db-fun db-put (key val)
+  (defun--db-fun create-db (&optional test-fun)
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    "Create a new hashtable on DB-SYM's DB-PROP property."
+    (let* ( (test-fun (or test-fun #'equal))
+            (new-table (make-hash-table :test test-fun)))
+      (put db-sym db-prop new-table)))
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Put VAL in the hashtable on DB-SYM's DB-PROP property under KEY."
-  (setf (gethash key db) val))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that (db-put 'foo 'bar 777) returns 777)
-(confirm that (db-put 'foo 'bar 888 'alt) returns 888)
-(confirm that (db-get 'foo 'bar) returns (777 . t))
-(confirm that (db-get 'foo 'bar 'alt) returns (888 . t))
-(confirm that (db-put 'foo 'baz nil) returns nil)
-(confirm that (db-get 'foo 'baz) returns (nil . t))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun--db-fun db-clear ()
+  (confirm that (hash-table-p (create-db foo 'db #'eq)) returns t)
+  (confirm that (hash-table-p (create-db foo 'alt)) returns t)
+  (confirm that (hash-table-p (get foo 'db)) returns t)
+  (confirm that (hash-table-p (get foo 'alt)) returns t)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Clear the hashtable on DB-SYM's DB-PROP property."
-  (clrhash db))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(db-clear 'foo)
-(db-clear 'foo 'alt)
-(confirm that (db-get 'foo 'bar) returns (nil))
-(confirm that (db-get 'foo 'bar 'alt) returns (nil))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (defun--db-fun has-db? ()
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    "Return t if DB-SYM has a DB-PROP property."
+    (hash-table-p (get db-sym db-prop)))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (confirm that (has-db? 'bloop) returns nil)
+  (confirm that (has-db? foo) returns t)
+  (confirm that (has-db? foo 'alt) returns t)
+  (confirm that (has-db? foo 'nope) returns nil)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (defun--db-fun db-get (key)
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    "Get the value of KEY in the hashtable on DB-SYM's DB-PROP property."
+    (with-gensyms (db-not-found)
+      (let* ( (got (gethash key db db-not-found))
+              (found (not (eq got db-not-found)))
+              (val (when found got)))
+        (cons val found))))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (confirm that (db-get foo 'bar) returns (nil))
+  (confirm that (db-get foo 'bar 'alt) returns (nil))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (defun--db-fun db-put (key val)
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    "Put VAL in the hashtable on DB-SYM's DB-PROP property under KEY."
+    (setf (gethash key db) val))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (confirm that (db-put foo 'bar 777) returns 777)
+  (confirm that (db-put foo 'bar 888 'alt) returns 888)
+  (confirm that (db-get foo 'bar) returns (777 . t))
+  (confirm that (db-get foo 'bar 'alt) returns (888 . t))
+  (confirm that (db-put foo 'baz nil) returns nil)
+  (confirm that (db-get foo 'baz) returns (nil . t))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (defun--db-fun db-clear ()
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    "Clear the hashtable on DB-SYM's DB-PROP property."
+    (clrhash db))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (db-clear foo)
+  (db-clear foo 'alt)
+  (confirm that (db-get foo 'bar) returns (nil))
+  (confirm that (db-get foo 'bar 'alt) returns (nil))
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  ) ; end of with-gensyms.
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
