d622ccd897dfcec58122b24b38514b949d0d160c
Ariane Emory <ariane.emory@protonmail.com>
2024-04-21 14:53:54 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -3,17 +3,21 @@
 (require 'aris-fib-benchmarks)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (def* (div-mod (n : integer) (d : integer)) => (pair-of integer)
-;;   `(,(/ n d) . ,(% n d)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun munge-arglist (arglist prepend-required-args prepend-optional-args)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (prndiv)
+  (let* ( (arglist (cons :DUMMY arglist))
+          (pos arglist)
+          optionals)
+    (while pos
+      (when (eq (second pos) '&optional) ; no case for &rest yet, avoid it for now.
+        (setq optionals (cddr pos)) ; snip into two separate lists. 
+        (setcdr pos nil))
+      (pop pos))
+    (pop arglist) ; pop :DUMMY.
+    (let* ((arglist `(db-sym ,@arglist &optional db-prop ,@optionals)))
+      arglist)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; ;; ... expands to:
-;; (defun* div-mod ((n : integer) (d : integer)) => (pair-of integer)
-;;   `(,(/ n d) \,(% n d)))
-
-;; (div-mod 19 8) ;; => (2 . 3)
-;; (div-mod -19 8) ;; => (-2 . -3)
-;; (div-mod 19 -8) ;; => (-2 . 3)
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
+(munge-arglist '(x y &optional z) '(db-sym) '(db-prop))
