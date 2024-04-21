c927141fccc84484bf28c1a6e7e457bb8a99bf9e
Ariane Emory <ariane.emory@protonmail.com>
2024-04-21 19:57:00 -0400

    ...

--- lisp/aris-funs--sym-db.el
+++ lisp/aris-funs--sym-db.el
@@ -5,8 +5,9 @@
 (require 'aris-funs--prepend-new-args)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq foo (gensym "foo-"))
+(defvar --db-foo (gensym "foo-") "A symbol to use as a test DB.")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
@@ -25,16 +26,6 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun--db-fun require-db! ()
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Signal an error if DB-SYM does not have a DB-PROP property."
-  (unless (has-db? db-sym db-prop)
-    (error "No DB found on %s's %s property." db-sym db-prop)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun--db-fun create-db (&optional test-fun)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Create a new hashtable on DB-SYM's DB-PROP property."
@@ -42,30 +33,39 @@
           (new-table (make-hash-table :test test-fun)))
     (put db-sym db-prop new-table)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that (hash-table-p (create-db foo 'db #'eq)) returns t)
-(confirm that (hash-table-p (create-db foo 'alt)) returns t)
-(confirm that (hash-table-p (get foo 'db)) returns t)
-(confirm that (hash-table-p (get foo 'alt)) returns t)
+(confirm that (hash-table-p (create-db --db-foo 'db #'eq)) returns t)
+(confirm that (hash-table-p (create-db --db-foo 'alt)) returns t)
+(confirm that (hash-table-p (get --db-foo 'db)) returns t)
+(confirm that (hash-table-p (get --db-foo 'alt)) returns t)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun--db-fun has-db? ()
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Return t if DB-SYM has a DB-PROP property."
+  "Return t if DB-SYM has a hashtable on its DB-PROP property."
   (hash-table-p (get db-sym db-prop)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (confirm that (has-db? 'bloop) returns nil)
-(confirm that (has-db? foo) returns t)
-(confirm that (has-db? foo 'alt) returns t)
-(confirm that (has-db? foo 'nope) returns nil)
+(confirm that (has-db? --db-foo) returns t)
+(confirm that (has-db? --db-foo 'alt) returns t)
+(confirm that (has-db? --db-foo 'nope) returns nil)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun--db-fun require-db! ()
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Signal an error if DB-SYM does not have hash table on its  DB-PROP property."
+  (unless (has-db? db-sym db-prop)
+    (error "No DB found on %s's %s property." db-sym db-prop)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun--db-fun ensure-db! ()
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Create a new DB on DB-SYM's DB-PROP property if it doesn't already exist."
+  "Create a new hashtable on DB-SYM's DB-PROP property if it doesn't already exist."
   (unless (has-db? db-sym db-prop)
     (create-db db-sym db-prop)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -87,8 +87,8 @@
             (val (when found got)))
       (cons val found))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that (db-get foo 'bar) returns (nil))
-(confirm that (db-get foo 'bar 'alt) returns (nil))
+(confirm that (db-get --db-foo 'bar) returns (nil))
+(confirm that (db-get --db-foo 'bar 'alt) returns (nil))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
@@ -99,12 +99,12 @@
   (require-db! db-sym db-prop)
   (setf (gethash key db) val))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that (db-put foo 'bar 777) returns 777)
-(confirm that (db-put foo 'bar 888 'alt) returns 888)
-(confirm that (db-get foo 'bar) returns (777 . t))
-(confirm that (db-get foo 'bar 'alt) returns (888 . t))
-(confirm that (db-put foo 'baz nil) returns nil)
-(confirm that (db-get foo 'baz) returns (nil . t))
+(confirm that (db-put --db-foo 'bar 777) returns 777)
+(confirm that (db-put --db-foo 'bar 888 'alt) returns 888)
+(confirm that (db-get --db-foo 'bar) returns (777 . t))
+(confirm that (db-get --db-foo 'bar 'alt) returns (888 . t))
+(confirm that (db-put --db-foo 'baz nil) returns nil)
+(confirm that (db-get --db-foo 'baz) returns (nil . t))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
@@ -115,10 +115,10 @@
   (require-db! db-sym db-prop)
   (clrhash db))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(db-clear foo)
-(db-clear foo 'alt)
-(confirm that (db-get foo 'bar) returns (nil))
-(confirm that (db-get foo 'bar 'alt) returns (nil))
+(db-clear --db-foo)
+(db-clear --db-foo 'alt)
+(confirm that (db-get --db-foo 'bar) returns (nil))
+(confirm that (db-get --db-foo 'bar 'alt) returns (nil))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
