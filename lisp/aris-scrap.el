f27b8ddacb778a7caa101ed19338395cb2795250
Ariane Emory <ariane.emory@protonmail.com>
2024-04-22 14:54:38 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -5,6 +5,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (dolist (thing '(1 2 ,3 4 ,@5 #'6 '7 `8))
   (cond
     ((eq 'quote (car-safe thing)) (prn "This one is kind of special: %s" (cadr thing)))
@@ -13,9 +14,13 @@
     ((eq 'function (car-safe thing)) (prn "This one is super special: %s" (cadr thing)))
     ((eq '\` (car-safe thing)) (prn "This one is extra special: %s" (cadr thing)))
     (t (prn "%s" thing))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; my integer class:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define-class integer (value) nil
   (val () value)
   (add (other) (integer (+ value (val other))))
@@ -32,56 +37,27 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun transform-tree2 (pred? fun tree)
-  "Replace atoms matching PRED? in TREE with the result of applying FUN to them."
-  (cond
-    ((listp tree) (mapr tree
-                    (lambda (item)
-                      (cond
-                        ((listp item) (transform-tree2 pred? fun item))
-                        (t (cond
-                             ((funcall pred? item) (funcall fun item))
-                             (t item)))))))
-    (t tree)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that (transform-tree #'even? #'double '(1 2 3 4 5 6 7 (8 5 9 5 10)))
-  returns (1 4 3 8 5 12 7 (16 5 9 5 20)))
-(confirm that (transform-tree2 #'even? #'double '(1 2 3 4 5 6 7 (8 5 9 5 10)))
-  returns (1 4 3 8 5 12 7 (16 5 9 5 20)))
-(confirm that (transform-tree3 #'even? #'double '(1 2 3 4 5 6 7 (8 5 9 5 10)))
-  returns (1 4 3 8 5 12 7 (16 5 9 5 20)))
-(confirm that (transform-tree4 #'even? #'double '(1 2 3 4 5 6 7 (8 5 9 5 10)))
-  returns (1 4 3 8 5 12 7 (16 5 9 5 20)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(benchmark-run 10000 (transform-tree #'even? #'double '(1 2 3 4 5 6 7 (8 5 9 5 10)))) ;; => (0.12520199999999998 1 0.059885000000008404
-(benchmark-run 10000 (transform-tree2 #'even? #'double '(1 2 3 4 5 6 7 (8 5 9 5 10)))) ;; => (0.6730539999999999 8 0.46613700000000335(benchmark-run 10000 (transform-tree3 #'even? #'double '(1 2 3 4 5 6 7 (8 5 9 5 10)))) ;; => (0.595613 7 0.40057899999999336
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun op-to-name (op)
-  (if (symbolp op)
-    (cl-case op
-      (+ 'add)
-      (- 'sub)
-      (* 'mul)
-      (/ 'div)
-      (% 'rem))
-    (integer op)))
+  "If OP is an operator namin one of integer's methods, return the method name."
+  (cl-case op
+    (+ 'add)
+    (- 'sub)
+    (* 'mul)
+    (/ 'div)
+    (% 'rem)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(benchmark-run 10000 (transform-tree #'symbolp #'op-to-name '(2 + (3 * 4) / 5 % 6))) ;; => (1.443532 18 0.9447859999999935
-(benchmark-run 10000 (transform-tree #'always #'op-to-name '(2 + (3 * 4) / 5 % 6))) ;; => (1.913227 24 1.2899850000000015
 
 
+;; (transform-tree #'always
+;;   (lambda (x) (pcase (integerp x)
+;;            (integer x)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define-class integer (value) nil
-  (val () value)
-  (add (other) (integer (+ value (val other))))
-  (sub (other) (integer (- value (val other))))
-  (mul (other) (integer (* value (val other))))
-  (div (other) (integer (/ value (val other))))
-  (rem (other) (integer (% value (val other)))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that (val (mul (integer 2) (add (integer 5) (integer 7))))
-  returns 24)
-(confirm that (val (rem (mul (integer 2) (add (integer 5) (integer 7))) (integer 5)))
-  returns 4)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun transform-fun (expr)
+  (pcase expr
+    ((and (cl-type integer) int) (integer int))
+    ((and (let op-name (op-to-name expr)) op-name) op-name)
+    (value value)))
+
+(transform-fun 8)
+(transform-fun 'foo)
+(transform-fun '+)
