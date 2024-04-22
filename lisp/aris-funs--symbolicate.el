1378807539913bf588f243a7f0007e10d19f432a
Ariane Emory <ariane.emory@protonmail.com>
2024-04-22 05:58:16 -0400

    ...

--- lisp/aris-funs--symbolicate.el
+++ lisp/aris-funs--symbolicate.el
@@ -1,6 +1,7 @@
 ;; -*- fill-column: 90; eval: (display-fill-column-indicator-mode 1); -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'aris-funs--confirm)
+(require 'aris-funs--lists) ; `compact'.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
@@ -8,11 +9,22 @@
 (defun symbolicate (&rest things)
   "A simplified substitute for Alexandria's `symbolicate' function: stringify the THINGS, join them with \"-\" and
 intern them to make a symbol."
-  (intern (string-join (mapcar (lambda (x) (format "%s" x)) things) "-")))
+  (intern (string-join (mapcar (lambda (x) (format "%s" x)) (compact things)) "-")))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (confirm that (symbolicate "foo") returns foo)
 (confirm that (symbolicate "foo" 'bar 1) returns foo-bar-1)
+(confirm that (symbolicate "foo" 'bar nil 1) returns foo-bar-1)
+(confirm that (symbolicate "asd" "" 1) returns asd---1)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun gensymbolicate (&rest things)
+  "`symbolicate' THINGS and use the result as the argument to `gensym'.
+Example:
+(gensymbolicate \"foo\" 'bar 1) ⇒ foo-bar-11533"
+  (gensym (apply #'symbolicate `(,@things ""))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
