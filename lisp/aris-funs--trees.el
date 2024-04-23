7fe2da680b417ef9714908d2b3c69bc3762f1504
Ariane Emory <ariane.emory@protonmail.com>
2024-04-23 00:27:00 -0400

    ...

--- lisp/aris-funs--trees.el
+++ lisp/aris-funs--trees.el
@@ -7,7 +7,7 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun transform-tree (pred? fun tree)
+(defun transform-tree (pred? fun tree &optional swap-test-order)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Replace atoms matching PRED? in TREE with the result of applying FUN to them."
   (let (result tail)
@@ -16,12 +16,13 @@
               (new-tail
                 (list
                   (cond
+                    ((and swap-test-order (funcall pred? head))
+                      (funcall fun head))
                     ((cons? head)
                       (transform-tree pred? fun head))
-                    ((funcall pred? head)
+                    ((and (not swap-test-order) (funcall pred? head))
                       (funcall fun head))
-                    (t
-                      head)))))
+                    (t head)))))
         (if result
           (setcdr tail new-tail)
           (setq result new-tail))
@@ -30,8 +31,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (confirm that (transform-tree #'even? #'double '(1 2 3 4 5 6 7 (8 5 9 5 10)))
   returns (1 4 3 8 5 12 7 (16 5 9 5 20)))
+(confirm that
+  (transform-tree
+    (lambda (x) (equal x '(1 2 3)))
+    (lambda (x) '(a b c))
+    '(1 2 3 4 5 6 (1 2 3) 7 8 9 10)
+    t)
+  returns (1 2 3 4 5 6 (a b c) 7 8 9 10))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
