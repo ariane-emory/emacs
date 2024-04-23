ecd6d28384a094e3066118134cc63519e0402769
Ariane Emory <ariane.emory@protonmail.com>
2024-04-23 19:40:46 -0400

    ...

--- lisp/aris-scrap.el
+++ lisp/aris-scrap.el
@@ -3,29 +3,6 @@
 (require 'aris-funs--trees)
 (require 'peter-norvigs-funs--objects)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(let ((seed (car (current-time))))
-  (defun myrand (limit)
-    "Not a very good random number generator."
-    (lsh (1- (% (setq seed (logand #xffff
-                             ((lambda (z) (if (positive? z) z (+ z -32768)))
-                               (* seed 899.))))
-               (lsh limit 1)))
-      -1)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(ignore!
-  (let ((limit 10)
-         (counts '( (0 . 0) (1 . 0) (2 . 0) (3 . 0) (4 . 0)
-                    (5 . 0) (6 . 0) (7 . 0) (8 . 0) (9 . 0))))
-    (dotimes (x 2000000)
-      (let ((roll (myrand limit)))
-        (alist-put! roll counts (1+ (alist-get roll counts 0)))))
-    (dolist (count counts)
-      (prn "%s" count))))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 
 (n:defclass n:integer (value) nil
   (val ()      value)
