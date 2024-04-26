6155c141aa3d8bc3b237798fa566a98e55c3761e
Ariane Emory <ariane.emory@protonmail.com>
2024-04-26 19:56:10 -0400

    ...

--- lisp/aris-scrap2.el
+++ lisp/aris-scrap2.el
@@ -6,6 +6,19 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(dolist (thing '(1 2 ,3 4 ,@5 #'6 '7 `8))
+  (cond
+    ((eq 'quote (car-safe thing)) (prn "This one is kind of special: %s" (cadr thing)))
+    ((eq '\, (car-safe thing)) (prn "This one is special: %s" (cadr thing)))
+    ((eq '\,@ (car-safe thing)) (prn "This one is very special: %s" (cadr thing)))
+    ((eq 'function (car-safe thing)) (prn "This one is super special: %s" (cadr thing)))
+    ((eq '\` (car-safe thing)) (prn "This one is extra special: %s" (cadr thing)))
+    (t (prn "%s" thing))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Manually tweaked expansion with a private method:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; (let
@@ -42,6 +55,91 @@
 
 
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (defmacro switch (value &rest body)
+;;   "Steele's `switch' from 'The Evolution of Lisp'."
+;;   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;   (let* ( (newbody (cl-mapcar #'(lambda (clause)
+;;                                   `(,(gensym) ,@(rest clause)))
+;;                      body))
+;;           (switcher (cl-mapcar #'(lambda (clause newclause)
+;;                                    `(,(first clause) (go ,(first newclause))))
+;;                       body newbody)))
+;;     `(cl-block switch
+;;        (cl-tagbody (cl-case ,value ,@switcher)
+;;          (break)
+;;          ,@(apply #'nconc newbody)))))
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (defmacro break () '(cl-return-from switch))
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (confirm that
+;;   (let (res)
+;;     (switch 0
+;;       (0 (push "none" res) (break))
+;;       (1 (push "one " res))
+;;       (2 (push "too " res))
+;;       (3 (push "many" res)))
+;;     (nreverse res))
+;;   returns ("none"))
+;; (confirm that
+;;   (let (res)
+;;     (switch 1
+;;       (0 (push "none" res) (break))
+;;       (1 (push "one " res))
+;;       (2 (push "too " res))
+;;       (3 (push "many" res)))
+;;     (nreverse res))
+;;   returns ("one " "too " "many"))
+;; (confirm that
+;;   (let (res)
+;;     (switch 2
+;;       (0 (push "none" res) (break))
+;;       (1 (push "one " res))
+;;       (2 (push "too " res))
+;;       (3 (push "many" res)))
+;;     (nreverse res))
+;;   returns ("too " "many"))
+;; (confirm that
+;;   (let (res)
+;;     (switch 3
+;;       (0 (push "none" res) (break))
+;;       (1 (push "one " res))
+;;       (2 (push "too " res))
+;;       (3 (push "many" res)))
+;;     (nreverse res))
+;;   returns ("many"))
+;; (confirm that
+;;   (let (res)
+;;     (switch 4
+;;       (0 (push "none" res) (break))
+;;       (1 (push "one " res))
+;;       (2 (push "too " res))
+;;       (3 (push "many" res)))
+;;     (nreverse res))
+;;   returns nil)
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (let (res)  
+;;   (cl-block switch
+;;     (cl-tagbody
+;;       (cl-case 1
+;;         (0 (go g1471))
+;;         (1 (go g1472))
+;;         (2 (go g1473))
+;;         (3 (go g1474)))
+;;       (break)
+;;       g1471
+;;       (push "none" res)
+;;       (break)
+;;       g1472
+;;       (push "one " res)
+;;       g1473
+;;       (push "too " res)
+;;       g1474
+;;       (push "many" res)))
+;;   (nreverse res))
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defmacro switch (value &rest body)
   "Steele's `switch' from 'The Evolution of Lisp'."
@@ -59,73 +157,46 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defmacro break () '(cl-return-from switch))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that
-  (let (res)
-    (switch 0
-      (0 (push "none" res) (break))
-      (1 (push "one " res))
-      (2 (push "too " res))
-      (3 (push "many" res)))
-    (nreverse res))
-  returns ("none"))
-(confirm that
-  (let (res)
-    (switch 1
-      (0 (push "none" res) (break))
-      (1 (push "one " res))
-      (2 (push "too " res))
-      (3 (push "many" res)))
-    (nreverse res))
-  returns ("one " "too " "many"))
-(confirm that
-  (let (res)
-    (switch 2
-      (0 (push "none" res) (break))
-      (1 (push "one " res))
-      (2 (push "too " res))
-      (3 (push "many" res)))
-    (nreverse res))
-  returns ("too " "many"))
-(confirm that
-  (let (res)
-    (switch 3
-      (0 (push "none" res) (break))
-      (1 (push "one " res))
-      (2 (push "too " res))
-      (3 (push "many" res)))
-    (nreverse res))
-  returns ("many"))
-(confirm that
-  (let (res)
-    (switch 4
-      (0 (push "none" res) (break))
-      (1 (push "one " res))
-      (2 (push "too " res))
-      (3 (push "many" res)))
-    (nreverse res))
-  returns nil)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(let (res)  
-  (cl-block switch
-    (cl-tagbody
-      (cl-case 1
-        (0 (go g1471))
-        (1 (go g1472))
-        (2 (go g1473))
-        (3 (go g1474)))
-      (break)
-      g1471
-      (push "none" res)
-      (break)
-      g1472
-      (push "one " res)
-      g1473
-      (push "too " res)
-      g1474
-      (push "many" res)))
+(let (res)
+  (switch 0
+    (0 (push "none" res) (break))
+    (1 (push "one " res))
+    (2 (push "too " res))
+    (3 (push "many" res)))
   (nreverse res))
+
+(let (res)
+  (switch 1
+    (0 (push "none" res) (break))
+    (1 (push "one " res))
+    (2 (push "too " res))
+    (3 (push "many" res)))
+  (nreverse res))
+
+(let (res)
+  (switch 2
+    (0 (push "none" res) (break))
+    (1 (push "one " res))
+    (2 (push "too " res))
+    (3 (push "many" res)))
+  (nreverse res))
+
+(let (res)
+  (switch 3
+    (0 (push "none" res) (break))
+    (1 (push "one " res))
+    (2 (push "too " res))
+    (3 (push "many" res)))
+  (nreverse res))
+
+(let (res)
+  (switch 4
+    (0 (push "none" res) (break))
+    (1 (push "one " res))
+    (2 (push "too " res))
+    (3 (push "many" res)))
+  (nreverse res))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
 
 
