3d4692c007c4954572080388f716769ae0d5daf2
Ariane Emory <ariane.emory@protonmail.com>
2024-04-26 20:32:48 -0400

    ...

---
+++ lisp/guy-steeles-funs--switch.el
@@ -0,0 +1,81 @@
+;; -*- fill-column: 90; eval: (display-fill-column-indicator-mode 1); -*-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; `switch' macro presented by Guy Steele, with some minor adjustments.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'aris-funs--confirm)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro switch (value &rest body)
+  "Steele's `switch' from 'The Evolution of Lisp' (but with the originally-separate
+`break' macro refactored to use `cl-macrolet' and a `gensym'-ed block label ."
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (let* ( (label    (gensym "switch-"))
+          (newbody  (mapcar #'(lambda (clause) `(,(gensym) ,@(rest clause))) body))
+          (switcher (cl-mapcar
+                      #'(lambda (clause newclause) `(,(first clause) (go ,(first newclause))))
+                      body newbody)))
+    `(cl-block ,label
+       (cl-macrolet ((break () '(cl-return-from ,label)))
+         (cl-tagbody (cl-case ,value ,@switcher)
+           (break)
+           ,@(apply #'nconc newbody))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (defmacro break () '(cl-return-from switch))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(confirm that
+  (let (res)
+    (switch 0
+      (0 (push "none" res) (break))
+      (1 (push "one " res))
+      (2 (push "too " res))
+      (3 (push "many" res)))
+    (nreverse res))
+  returns ("none"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(confirm that
+  (let (res)
+    (switch 1
+      (0 (push "none" res) (break))
+      (1 (push "one " res))
+      (2 (push "too " res))
+      (3 (push "many" res)))
+    (nreverse res))
+  returns ("one " "too " "many"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(confirm that
+  (let (res)
+    (switch 2
+      (0 (push "none" res) (break))
+      (1 (push "one " res))
+      (2 (push "too " res))
+      (3 (push "many" res)))
+    (nreverse res))
+  returns ("too " "many"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(confirm that
+  (let (res)
+    (switch 3
+      (0 (push "none" res) (break))
+      (1 (push "one " res))
+      (2 (push "too " res))
+      (3 (push "many" res)))
+    (nreverse res))
+  returns ("many"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(confirm that
+  (let (res)
+    (switch 4
+      (0 (push "none" res) (break))
+      (1 (push "one " res))
+      (2 (push "too " res))
+      (3 (push "many" res)))
+    (nreverse res))
+  returns nil)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(provide 'guy-steeles-funs--switch)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
