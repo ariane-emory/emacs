a96870b625c36346549be87df447076d5dad1354
Ariane Emory <ariane.emory@protonmail.com>
2024-04-29 17:58:59 -0400

    sketchy

--- lisp/nolex.el
+++ lisp/nolex.el
@@ -12,7 +12,8 @@
   '( (i . you)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defvar patterns
-  '((,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun)))
+  '( (,desig1 ,verb1 that ,desig2 ,modal-verb never ,verb2 a ,noun)
+     (,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (prndiv)
 (let ((pattern '(,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun)))
@@ -22,9 +23,12 @@
                (i know that i could have a smoke)
                (i believe that you have seen a ghost)
                (you believe that i have seen a ghost)
-               (i suspect that you have (never seen) a (red car))))
-    (when-let-alist (easy-match pattern target)
-      (prn
-        (flatten `( do ,(swap-sym .desig1) really ,.verb1 that ,(swap-sym .desig2)
-                    ,.modal-verb ,.verb2 a ,.noun \?))))))
+               (i suspect that you have never seen a zebra)))
+    (catch 'matched
+      (dolist (pattern patterns)
+        (when-let-alist (easy-match pattern target)
+          (prn
+            `( do ,(swap-sym .desig1) really ,.verb1 that ,(swap-sym .desig2)
+               ,.modal-verb ,.verb2 a ,.noun \?))
+          (throw 'matched nil))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
