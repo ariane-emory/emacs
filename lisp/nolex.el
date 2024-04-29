2ad586d3ae9efd320412b5607d5af8cf60a197b2
Ariane Emory <ariane.emory@protonmail.com>
2024-04-29 14:12:18 -0400

    ...

--- lisp/nolex.el
+++ lisp/nolex.el
@@ -1,26 +1,28 @@
 (defun foo (pat target)
+  "A very bad pattern matching fun."
   (let (alist)
     (while-let ( (pat-head  (pop pat))
                  (targ-head (pop target))
                  (_ (or (equal pat-head targ-head) (eq '\, (car-safe pat-head)))))
-      (when (eq '\, (car-safe pat-head))
+      (when (eq '\, (car-safe pat-head)) ; pat-head is a variable
         (setf alist (cons (cons (cadr pat-head) targ-head) alist))))
     (unless (or pat target) (nreverse alist))))
 
-(when-let ((alist (foo '(i ,blah ,verb a ,thing) '(i would like a smoke))))
-  (let-alist alist
-    (prn `(why do you think that you ,.blah ,.verb a ,.thing \?))))
-
-(when-let ((alist (foo '(i ,blah ,verb a ,thing) '(i could have a smoke))))
-  (let-alist alist
-    (prn `(why do you think that you ,.blah ,.verb a ,.thing \?))))
-
-(when-let ((alist (foo '(i ,blah ,verb a ,thing) '(i have seen a ghost))))
-  (let-alist alist
-    (prn `(why do you think that you ,.blah ,.verb a ,.thing \?))))
-
-(when-let-alist (foo '(i ,blah ,verb a ,thing) '(i have (never seen) a (red car)))
-  (prn (flatten `(why do you think that you ,.blah ,.verb a ,.thing \?))))
-
-
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(when-let-alist (foo '(i ,modal-verb ,verb a ,thing) '(i have (never seen) a (red car)))
+  (prndiv)
+  (prn (flatten `(why do you think that you ,.modal-verb ,.verb a ,.thing \?))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(dolist (pair
+          '( ((i ,modal-verb ,verb a ,thing) . (i would like a smoke))
+             ((i ,modal-verb ,verb a ,thing) . (i could have a smoke))
+             ((i ,modal-verb ,verb a ,thing) . (i have seen a ghost))
+             ((i ,modal-verb ,verb a ,thing) . (i have (never seen) a (red car)))))
+  (let-kvp pair
+    (prndiv)
+    (prn "pat:   %s" .key)
+    (prn "targ:  %s" .val)
+    (when-let-alist (foo .key .val)
+      (prn
+        (flatten `(Do you really believe that you ,.modal-verb ,.verb a ,.thing \?))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
