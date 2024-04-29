a0b18d95593b89261251018c229bb6d7e8496e2a
Ariane Emory <ariane.emory@protonmail.com>
2024-04-29 17:27:02 -0400

    ...

--- lisp/nolex.el
+++ lisp/nolex.el
@@ -1,13 +1,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(let ((pattern '(i ,verb1 i ,modal-verb ,verb2 a ,thing)))
+(let ((pattern '(i ,verb1 ,desig ,modal-verb ,verb2 a ,thing)))
   (dolist (target
             '( (i think i would like a smoke)
                (i know i could have a smoke)
                (i believe i have seen a ghost)
-               (i suspect i have (never seen) a (red car))))
+               (i suspect you have (never seen) a (red car))))
     ;; (prn "pattern: %s" pattern)
     ;; (prn "target:  %s" target)
     (when-let-alist (easy-match pattern target)
       (prn
-        (flatten `(do you really ,.verb1 that you ,.modal-verb ,.verb2 a ,.thing \?))))))
+        (flatten `(do you really ,.verb1 that ,.desig ,.modal-verb ,.verb2 a ,.thing \?))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
