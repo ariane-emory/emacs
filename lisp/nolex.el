a83919dd9ffe4fc67c0f74426569af6e6dd8c5e8
Ariane Emory <ariane.emory@protonmail.com>
2024-04-29 17:54:50 -0400

    ...

--- lisp/nolex.el
+++ lisp/nolex.el
@@ -11,17 +11,20 @@
 (defvar swap-syms
   '( (i . you)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defvar patterns
+  '((,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (prndiv)
-(let ((pattern '(,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun)))
-  (dolist (target
-            '( (i think that i would like a smoke)
-               (i think that you would like a smoke)
-               (i know that i could have a smoke)
-               (i believe that you have seen a ghost)
-               (you believe that i have seen a ghost)
-               (i suspect that you have (never seen) a (red car))))
-    (when-let-alist (easy-match pattern target)
-      (prn
-        (flatten `( do ,(swap-sym .desig1) really ,.verb1 that ,(swap-sym .desig2)
-                    ,.modal-verb ,.verb2 a ,.noun \?))))))
+  (let ((pattern '(,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun)))
+    (dolist (target
+              '( (i think that i would like a smoke)
+                 (i think that you would like a smoke)
+                 (i know that i could have a smoke)
+                 (i believe that you have seen a ghost)
+                 (you believe that i have seen a ghost)
+                 (i suspect that you have (never seen) a (red car))))
+      (when-let-alist (easy-match pattern target)
+        (prn
+          (flatten `( do ,(swap-sym .desig1) really ,.verb1 that ,(swap-sym .desig2)
+                      ,.modal-verb ,.verb2 a ,.noun \?))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
