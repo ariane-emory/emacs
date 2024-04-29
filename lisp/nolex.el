ef0c6772cce6942f6a3d3fedbe39802690697be6
Ariane Emory <ariane.emory@protonmail.com>
2024-04-29 17:33:43 -0400

    wip

--- lisp/nolex.el
+++ lisp/nolex.el
@@ -1,4 +1,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defvar swap-syms
+  '( (i . you)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun swap-sym (sym)
+  (let ((res
+          (cond
+            ((assoc sym swap-syms) (cdr (assoc sym swap-syms)))
+            ((rassoc sym swap-syms) (cdr (rassoc sym swap-syms)))
+            (t sym))))
+    (prn "swap %s for %s" sym res)
+    res))
+(swap-sym 'i)
+(swap-sym 'you)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (let ((pattern '(i ,verb1 ,desig ,modal-verb ,verb2 a ,thing)))
   (dolist (target
             '( (i think i would like a smoke)
@@ -9,5 +23,5 @@
     ;; (prn "target:  %s" target)
     (when-let-alist (easy-match pattern target)
       (prn
-        (flatten `(do you really ,.verb1 that ,.desig ,.modal-verb ,.verb2 a ,.thing \?))))))
+        (flatten `(do you really ,.verb1 that ,(swap-sym .desig) ,.modal-verb ,.verb2 a ,.thing \?))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
