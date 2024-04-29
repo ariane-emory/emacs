b51935596ffce5a56dd7236551735fc78ba7a0b4
Ariane Emory <ariane.emory@protonmail.com>
2024-04-29 19:59:26 -0400

    ...

--- lisp/nolex.el
+++ lisp/nolex.el
@@ -1,26 +1,46 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun swap-sym (sym)
-  (let ((res
-          (cond
-            ((assoc sym swap-syms) (cdr (assoc sym swap-syms)))
-            ((rassoc sym swap-syms) (car (rassoc sym swap-syms)))
-            (t sym))))
-    ;; (prn "swap %s for %s" sym res)
-    res))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defvar swap-syms
+(defvar *swap-words*
   '( (i . you)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defvar rules
-  '( ((,desig1 ,verb1 that ,desig2 ,modal-verb never ,verb2 a ,noun) .
-       ( do ,desig1 really ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun \?))
-     ;; (,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun)
-     ))
+(defvar *rules*
+  '( ( (,desig1 ,verb1 that ,desig2 ,modal-verb never ,verb2 a ,noun) .
+       ( do ,desig1 really ,verb1 that ,desig2 ,modal-verb never ,verb2 a ,noun \?))
+     ( (,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun) .
+       ( do ,desig1 really ,verb1 that ,desig2 ,modal-verb  ,verb2 a ,noun \?))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun swap-word (sym)
+  (cond
+    ((assoc sym  *swap-words*) (cdr (assoc sym *swap-words*)))
+    ((rassoc sym *swap-words*) (car (rassoc sym *swap-words*)))
+    (t sym)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun proc-input (target)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (prndiv)
+  (prn "target: %s" target)
+  (catch 'matched
+    (dolist (pair *rules*)
+      (let ((pattern (car pair)) (out-pattern (cdr pair)))
+        ;;(prn "try pat: %s" pattern)
+        (when-let ((alist (ap:match pattern target)))
+          (prn
+            (ap:fill out-pattern (rmapcar alist
+                                   (lambda (kvp)
+                                     (cons (car kvp) (swap-word (cdr kvp)))))))
+          (throw 'matched t))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (prndiv)
+(prn "START:")
 (dolist (target
           '( (i think that i would like a smoke)
              (i think that you would like a smoke)
@@ -28,19 +48,7 @@
              (i believe that you have seen a ghost)
              (you believe that i have seen a ghost)
              (i suspect that you have never seen a zebra)))
-  (catch 'matched
-    (dolist (pair rules)
-      (let ((pattern (car pair)) (out-pattern (cdr pair)))
-        (when-let ((alist (ap:match pattern target)))
-          (prn
-            (ap:fill out-pattern (rmapcar alist (lambda (kvp)
-                                                  (prn "kvp %s" kvp)
-                                                  (cons (car kvp)
-                                                    (swap-sym (cdr kvp))))))
-            ;; `( do ,(swap-sym .desig1) really ,.verb1 that ,(swap-sym .desig2)
-            ;;    ,.modal-verb ,.verb2 a ,.noun \?)
-            )
-          (throw 'matched nil))
-        ))))
+  (proc-input target))
+(prndiv)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
