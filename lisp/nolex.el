28dc6907ae3d546c6857cca8e266ea9aa94d7db7
Ariane Emory <ariane.emory@protonmail.com>
2024-04-29 19:30:37 -0400

    ...

--- lisp/nolex.el
+++ lisp/nolex.el
@@ -11,25 +11,31 @@
 (defvar swap-syms
   '( (i . you)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defvar patterns
-  '( (,desig1 ,verb1 that ,desig2 ,modal-verb never ,verb2 a ,noun)
-     (,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun)))
+(defvar rules
+  '( ((,desig1 ,verb1 that ,desig2 ,modal-verb never ,verb2 a ,noun) .
+       ( do ,(swap-sym .desig1) really ,.verb1 that ,(swap-sym .desig2) ,.modal-verb ,.verb2 a ,.noun \?))
+     ;; (,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun)
+     ))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (prndiv)
-(let ((pattern '(,desig1 ,verb1 that ,desig2 ,modal-verb ,verb2 a ,noun)))
-  (dolist (target
-            '( (i think that i would like a smoke)
-               (i think that you would like a smoke)
-               (i know that i could have a smoke)
-               (i believe that you have seen a ghost)
-               (you believe that i have seen a ghost)
-               (i suspect that you have never seen a zebra)))
-    (catch 'matched
-      (dolist (pattern patterns)
+(dolist (target
+          '( (i think that i would like a smoke)
+             (i think that you would like a smoke)
+             (i know that i could have a smoke)
+             (i believe that you have seen a ghost)
+             (you believe that i have seen a ghost)
+             (i suspect that you have never seen a zebra)))
+  (catch 'matched
+    (dolist (pair rules)
+      (let ((pattern (car pair)) (out-pattern (cdr pair)))
         (when-let-alist (ap:match pattern target)
           (prn
             `( do ,(swap-sym .desig1) really ,.verb1 that ,(swap-sym .desig2)
                ,.modal-verb ,.verb2 a ,.noun \?))
-          (throw 'matched nil))))))
+          (throw 'matched nil))
+        ))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
