f0b43b384bd15ce504d201db4415a549c82a1afc
Ariane Emory <ariane.emory@protonmail.com>
2024-05-01 14:58:39 -0400

    ...

--- lisp/aris-funs--trees.el
+++ lisp/aris-funs--trees.el
@@ -38,26 +38,50 @@
     '(1 2 3 4 5 6 (1 2 3) 7 8 9 10)
     t)
   returns (1 2 3 4 5 6 (a b c) 7 8 9 10))
+(confirm that ; do nothing case
+  (transform-tree
+    (lambda (x) (equal x '(1 2 3)))
+    (lambda (x) '(a b c))
+    '(1 2 3 4 5 6 (1 2 3) 7 8 9 10)
+    nil)
+  returns (1 2 3 4 5 6 (1 2 3) 7 8 9 10))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun transform-tree2 (pred? fun tree)
+(defun transform-tree2 (pred? fun tree &optional swap-test-order)
   "Replace atoms matching PRED? in TREE with the result of applying FUN to them.
 
 This version is slower than `transform-tree'."
   (if (listp tree)
     (rmapcar tree
       (lambda (item)
-        (if (listp item)
-          (transform-tree2 pred? fun item)
-          (if (funcall pred? item)
-            (funcall fun item)
-            item))))
+        (cond
+          ((and swap-test-order (funcall pred? item))
+            (funcall fun item))
+          ((cons? item)
+            (transform-tree2 pred? fun item swap-test-order))
+          ((and (not swap-test-order) (funcall pred? item))
+            (funcall fun item))
+          (t item))))
     tree))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (confirm that (transform-tree2 #'even? #'double '(1 2 3 4 5 6 7 (8 5 9 5 10)))
   returns (1 4 3 8 5 12 7 (16 5 9 5 20)))
+(confirm that
+  (transform-tree2
+    (lambda (x) (equal x '(1 2 3)))
+    (lambda (x) '(a b c))
+    '(1 2 3 4 5 6 (1 2 3) 7 8 9 10)
+    t)
+  returns (1 2 3 4 5 6 (a b c) 7 8 9 10))
+(confirm that ; do nothing case
+  (transform-tree2
+    (lambda (x) (equal x '(1 2 3)))
+    (lambda (x) '(a b c))
+    '(1 2 3 4 5 6 (1 2 3) 7 8 9 10)
+    nil)
+  returns (1 2 3 4 5 6 (1 2 3) 7 8 9 10))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
