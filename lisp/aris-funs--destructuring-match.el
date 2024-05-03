ef074941f7753265674fc46ce88964776d842efd
Ariane Emory <ariane.emory@protonmail.com>
2024-05-03 19:21:14 -0400

    ...

--- lisp/aris-funs--destructuring-match.el
+++ lisp/aris-funs--destructuring-match.el
@@ -162,8 +162,8 @@ KEY is already present in ALIST with a different value."
               ;; the loop successfully:
               ((and ellipsis (eq pat-head ellipsis))
                 (when pat-tail
-                  (error "ellipsis must be the last element in the pat-tail."))
-                (dm::prn "ELLIPSIS, discard %s." targ-tail)
+                  (error "ELLIPSIS must be the last element in the pat-tail."))                
+                (dm::prn-labeled targ-tail "DISCARD")
                 ;; nullify TARG-TAIL and PAT-TAIL:
                 (setf targ-tail nil)
                 (setf pat-tail  nil))
@@ -171,10 +171,10 @@ KEY is already present in ALIST with a different value."
               ;; the loop successfully:
               ((and unsplice (eq unsplice (car-safe pat-head)))
                 (when pat-tail
-                  (error "unsplice must be the last element in the pat-tail."))
+                  (error "UNSPLICE must be the last element in the pat-tail."))
                 (let ((var (cadr pat-head)))
                   (let ((target (cons targ-head targ-tail)))
-                    (dm::prn-labeled target "unsplicing")                    
+                    (dm::prn-labeled target "unsplicing")
                     ;; put the remainder of TARG-TAIL in VAR's key in ALIST:
                     (setf alist (dm::pushnew var alist target))
                     ;; nullify TARG-TAIL and PAT-TAIL:
@@ -240,7 +240,9 @@ KEY is already present in ALIST with a different value."
         (or alist t)))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (confirm that (dm:match '(w ,x ,y ,z) '(w 1 2 3)) returns ((x . 1) (y . 2) (z . 3)))
-(dm:match '(,x ,@ys) '(1 2 3 4));; (dm:match '(,x ... ,z) '(X 2 3))
+(dm:match '(,x ,@ys) '(1 2 3 4))
+(dm:match '(,x ...) '(1 2 3 4))
+;; (dm:match '(,x ... ,z) '(X 2 3))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; These are now all legal:
 ;; (dm:match '(,y (,y)) '(2 (3))) ; duplicate key in merge!
