34015cbc5b49aea648b8875716c51042e4db0274
Ariane Emory <ariane.emory@protonmail.com>
2024-05-03 15:41:31 -0400

    ...

--- lisp/aris-funs--destructuring-match.el
+++ lisp/aris-funs--destructuring-match.el
@@ -94,12 +94,13 @@ KEY is already present in ALIST with a different value."
         (if (not (consp alist))
           (dm::prn "ALIST:         %s" alist)
           (progn
-            (let ((pp-str (trim-trailing-whitespace (pp-to-string alist))))
+            (let ((pp-str (trim-trailing-whitespace
+                            (let (lisp-indent-offset)
+                              (pp-to-string alist)))))
               (if (<= (count-string-lines pp-str) 1)
                 (dm::prn "ALIST:         %s" alist)
                 (dm::prn "ALIST:")
-                (dm::prn "%s"
-                  (indent-string-lines pp-str))))))
+                (mapc #'prn (string-lines (indent-string-lines pp-str 2)))))))
         (dm::prn "pat-head:      %s" pat-head)
         (dm::prn "targ-head:     %s" targ-head)
         (dm::prn "pattern:       %s" pattern)
