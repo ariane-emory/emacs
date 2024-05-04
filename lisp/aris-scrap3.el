61388c0f8029c8464025efb2cd3a0630157825ef
Ariane Emory <ariane.emory@protonmail.com>
2024-05-03 23:40:55 -0400

    ...

---
+++ lisp/aris-scrap3.el
@@ -0,0 +1,15 @@
+;; -*- lexical-binding: t; fill-column: 100; eval: (display-fill-column-indicator-mode 1);  -*-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun dm::alist-putnew(key new-val alist)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Put NEW-VAL into ALIST as the association of KEY , throwing
+'no-match an association for KEY is already present in ALIST with a
+different (by `equal') value."
+  (when-let ( (assoc (assoc key alist))
+              (neq   (not (equal (cdr assoc) new-val))))
+    (throw 'no-match nil))
+  (cl-pushnew (cons key new-val) alist :test #'equal))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
