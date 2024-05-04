56346a71f723629c5cb3fb171551075f810994ea
Ariane Emory <ariane.emory@protonmail.com>
2024-05-04 05:47:18 -0400

    ...

--- lisp/aris-scrap3.el
+++ lisp/aris-scrap3.el
@@ -1,7 +1,11 @@
-;; -*- lexical-binding: t; fill-column: 100; eval: (display-fill-column-indicator-mode 1);  -*-
+;; -*- lexical-binding: nil; fill-column: 100; eval: (display-fill-column-indicator-mode 1);  -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
+(cl-macrolet ((foo () (prn "Foo!")))
+  (defun bar ()
+    (foo)))
 
+(bar)
 
