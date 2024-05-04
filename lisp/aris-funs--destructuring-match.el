c1252a46e88dfbc2ab18bf48c272337ebd308811
Ariane Emory <ariane.emory@protonmail.com>
2024-05-04 05:39:20 -0400

    ...

--- lisp/aris-funs--destructuring-match.el
+++ lisp/aris-funs--destructuring-match.el
@@ -47,7 +47,7 @@
   :group 'destructuring-match
   :type 'symbol)
 ;;---------------------------------------------------------------------------------------------------
-(defcustom *dm:enforce-final-position* nil
+(defcustom *dm:enforce-final-position* t
   "Whether or not dm:match should only allow ELLIPSIS and UNSPLICE in a pattern's final position."
   :group 'destructuring-match
   :type 'boolean)
