509d267a4b8b165d8820bd04b0764f261a4265e3
Ariane Emory <ariane.emory@protonmail.com>
2024-05-05 16:35:46 -0400

    ...

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
-  '(*dm:test-fill* nil)
+  '(*dm:test-fill* t)
   '(*dm:test-match* t)
   '(*pattern-dispatch--verbose* t)
   '(auto-encryption-mode nil)
