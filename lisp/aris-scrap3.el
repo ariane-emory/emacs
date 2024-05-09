13eef686de75898725891c9f6e24f0feda4dc8c3
Ariane Emory <ariane.emory@protonmail.com>
2024-05-09 01:07:44 -0400

    ...

--- lisp/aris-scrap3.el
+++ lisp/aris-scrap3.el
@@ -1,6 +1,26 @@
 ;; -*- lexical-binding: nil; fill-column: 100; eval: (display-fill-column-indicator-mode 1);  -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(dolist (thing  '( aa
+                   ,bb
+                   ,(bb t)
+                   ,@cc
+                   ,@(cc t)
+                   #'dd
+                   #'(dd t)
+                   'ee
+                   '(ee t)
+                   `ff
+                   `(ff t)
+                   )) ; '(1 2 ,3 4 ,@5 #'6 '7 `8))
+  (cond
+    ((eq '\, (car-safe thing)) (prn "This one is special: %s" thing))
+    ((eq '\,@ (car-safe thing)) (prn "This one is very special: %s" thing))
+    ((eq 'function (car-safe thing)) (prn "This one is super special: %s" thing))
+    ((eq 'quote (car-safe thing)) (prn "This one is kind of special: %s" thing))
+    ((eq '\` (car-safe thing)) (prn "This one is extra special: %s" thing))
+    ;; (t (prn "%s" thing))
+    ))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-(wordnut-lookup-synset "car" "n") 
