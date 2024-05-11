806e3e3995adb104fb9f804e9ce7bdbc487b2e1f
Ariane Emory <ariane.emory@protonmail.com>
2024-05-11 05:18:25 -0400

    ...

--- lisp/aris-funs--destructuring-match.el
+++ lisp/aris-funs--destructuring-match.el
@@ -231,7 +231,10 @@ KEY has a non-`equal' VAL in REFERENCE-ALIST."
   (dm::prndiv)
   (dm::prn "BEGIN MATCHING:       %S" pattern)
   (dm::prn "AGAINST:              %S" target)
-  (let* ( (result (with-indentation
+  (let* ( (pattern (dm::intern-pattern pattern))
+          ;; Currently, the next line will cause 2 tests to fail:
+          ;; (target  (dm::properize-target target))
+          (result (with-indentation
                     (dm::match1 pattern pattern target dont-care ellipsis unsplice nil nil)))
           (result (if (listp result) (nreverse result) result)))
     (dm::prn-labeled result "FINAL")
