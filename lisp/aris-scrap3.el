34d4669edb86eef1221635774f2c206b18686aa8
Ariane Emory <ariane.emory@protonmail.com>
2024-05-12 01:00:23 -0400

    ...

--- lisp/aris-scrap3.el
+++ lisp/aris-scrap3.el
@@ -2,120 +2,119 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(dolist (thing  '(  aa
-                   ,bb    ,(bb t)
-                   ,@cc  ,@(cc t)
-                   #'dd  #'(dd t)
-                   'ee    '(ee t)
-                   `ff    `(ff t)
-                   ))
-  (cond
-    ((eq '\, (car-safe thing)) (prn "This one is special: %s" thing))
-    ((eq '\,@ (car-safe thing)) (prn "This one is very special: %s" thing))
-    ((eq 'function (car-safe thing)) (prn "This one is super special: %s" thing))
-    ((eq 'quote (car-safe thing)) (prn "This one is kind of special: %s" thing))
-    ((eq '\` (car-safe thing)) (prn "This one is extra special: %s" thing))
-    ;; (t (prn "%s" thing))
-    ))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (dolist (thing  '(  aa
+;;                    ,bb    ,(bb t)
+;;                    ,@cc  ,@(cc t)
+;;                    #'dd  #'(dd t)
+;;                    'ee    '(ee t)
+;;                    `ff    `(ff t)
+;;                    ))
+;;   (cond
+;;     ((eq '\, (car-safe thing)) (prn "This one is special: %s" thing))
+;;     ((eq '\,@ (car-safe thing)) (prn "This one is very special: %s" thing))
+;;     ((eq 'function (car-safe thing)) (prn "This one is super special: %s" thing))
+;;     ((eq 'quote (car-safe thing)) (prn "This one is kind of special: %s" thing))
+;;     ((eq '\` (car-safe thing)) (prn "This one is extra special: %s" thing))
+;;     ;; (t (prn "%s" thing))
+;;     ))
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
-(setq lst '(1 2 3 4 5 6 7 8 9))
-(setq al (dm:match '(,x ,@ys) lst))
-(setcar (alist-get 'ys al) 999)
-(setq lst '(my name is ari))
-(setq al (dm:match '(my name is ,@name) lst))
-(setcar (alist-get 'name al) 'bob)
+;; (setq lst '(1 2 3 4 5 6 7 8 9))
+;; (setq al (dm:match '(,x ,@ys) lst))
+;; (setcar (alist-get 'ys al) 999)
+;; (setq lst '(my name is ari))
+;; (setq al (dm:match '(my name is ,@name) lst))
+;; (setcar (alist-get 'name al) 'bob)
 
 
-(dm::clear-compiled-patterns)
+;; (dm::clear-compiled-patterns)
 
-(dm:match
-  '(,a ,b (,c . ,d) . ,e)
-  (dm::properize-target '\. '(1 2 (3 . 4) . 5)))
+;; (dm:match
+;;   '(,a ,b (,c . ,d) . ,e)
+;;   (dm::properize-target '\. '(1 2 (3 . 4) . 5)))
 
-(symbol-plist '*dm*)
-
-
-(dm::unproperize!* '\. '(1 2 3 (4 (a b c (d \. e)) 5 6 \. 7) \. 8))
+;; (symbol-plist '*dm*)
 
 
-(let ((pat '(,a ,b . ,c)))
-  (prnl 2)
-  ;; this doesn't look correct intuitively due to wayward comma but i'm pretty sure it actually is:
-  (equal pat (dm::unproperize!* '\. (cl-copy-list (dm::compile-pattern '\. '\,@ '... '_ pat)))))
-
-(let ((pat ;; '(,w ,(x integer? . foo) . ,(y integer? . foo))
-        ;; '(,a ,(b integer? . foo) . ,c)
-        '(,a ,(b integer? foo) . ,(c integer? foo))
-        ))
-  (prnl 2)
-  (equal pat
-    (dm::unproperize!* '\. 
-      (cl-copy-list
-        (dm::compile-pattern '\. '\,@ '... '_
-          (dm::unproperize!* '\. 
-            (cl-copy-list
-              (dm::compile-pattern '\. '\,@ '... '_ 
-                (dm::unproperize!* '\. 
-                  (cl-copy-list
-                    (dm::compile-pattern '\. '\,@ '... '_ pat)))))))))))
-
-(symbol-plist '*dm*)
-
-(let ((pat ;; '(,w ,(x integer? . foo) . ,(y integer? . foo))
-        '(,a ,(b integer? . foo) . ,c)
-        ;; '(,a ,(b integer? foo) . ,(c integer? foo))
-        ))
-  (prnl 2)
-  (equal pat
-    (dm::unproperize!* '\.
-      (dm::compile-pattern '\. '\,@ '... '_ 
-        (dm::unproperize!* '\. 
-          (dm::compile-pattern '\. '\,@ '... '_ 
-            (dm::unproperize!* '\. 
-              (dm::compile-pattern '\. '\,@ '... '_  pat))))))))
+;; (dm::unproperize!* '\. '(1 2 3 (4 (a b c (d \. e)) 5 6 \. 7) \. 8))
 
 
-(dm::clear-compiled-patterns)
-(dm::compile-pattern '\. '\,@ '... '_ '(a b . c))
-(dm::unproperize!* '\. (dm::compile-pattern '\,@ '... '_ '\.  '(a b . c)))
-(dm::compile-pattern '\. '\,@ '... '_  '(a b . c))
+;; (let ((pat '(,a ,b . ,c)))
+;;   (prnl 2)
+;;   ;; this doesn't look correct intuitively due to wayward comma but i'm pretty sure it actually is:
+;;   (equal pat (dm::unproperize!* '\. (cl-copy-list (dm::compile-pattern '\. '\,@ '... '_ pat)))))
 
-(dm:match '(,@things . ,thing) '(one two three . four))
+;; (let ((pat ;; '(,w ,(x integer? . foo) . ,(y integer? . foo))
+;;         ;; '(,a ,(b integer? . foo) . ,c)
+;;         '(,a ,(b integer? foo) . ,(c integer? foo))
+;;         ))
+;;   (prnl 2)
+;;   (equal pat
+;;     (dm::unproperize!* '\. 
+;;       (cl-copy-list
+;;         (dm::compile-pattern '\. '\,@ '... '_
+;;           (dm::unproperize!* '\. 
+;;             (cl-copy-list
+;;               (dm::compile-pattern '\. '\,@ '... '_ 
+;;                 (dm::unproperize!* '\. 
+;;                   (cl-copy-list
+;;                     (dm::compile-pattern '\. '\,@ '... '_ pat)))))))))))
+
+;; (symbol-plist '*dm*)
+
+;; (let ((pat ;; '(,w ,(x integer? . foo) . ,(y integer? . foo))
+;;         '(,a ,(b integer? . foo) . ,c)
+;;         ;; '(,a ,(b integer? foo) . ,(c integer? foo))
+;;         ))
+;;   (prnl 2)
+;;   (equal pat
+;;     (dm::unproperize!* '\.
+;;       (dm::compile-pattern '\. '\,@ '... '_ 
+;;         (dm::unproperize!* '\. 
+;;           (dm::compile-pattern '\. '\,@ '... '_ 
+;;             (dm::unproperize!* '\. 
+;;               (dm::compile-pattern '\. '\,@ '... '_  pat))))))))
 
 
-(dm:match '(,@things . four)  '(one two three . four))
-(dm:match '(,@things four)  '(one two three four))
+;; (dm::clear-compiled-patterns)
+;; (dm::compile-pattern '\. '\,@ '... '_ '(a b . c))
+;; (dm::unproperize!* '\. (dm::compile-pattern '\,@ '... '_ '\.  '(a b . c)))
+;; (dm::compile-pattern '\. '\,@ '... '_  '(a b . c))
 
-(dm::clear-compiled-patterns)
-(symbol-plist '*dm*)
+;; (dm:match '(,@things . ,thing) '(one two three . four))
 
-(dm:match '(,@things) '(one two three . four))
-(dm:match '(,@things . _) '(one two three . four))
-(dm:match '(... . ,x) '(one two three . four))
 
-;; this seems okay to permit?
-(dm:match '(,@things . (three four))  '(one two three four))
+;; (dm:match '(,@things . four)  '(one two three . four))
+;; (dm:match '(,@things four)  '(one two three four))
 
-;; make these illegal, probably?
-(dm:match '(,@things . ,@zs) '(one two three four))
-(dm:match '(,@things . ,@zs) '(one two three . four))
+;; (dm::clear-compiled-patterns)
+;; (symbol-plist '*dm*)
 
-(dm:match '(,@things . ...)  '(one two three four))
-(dm:match '(,@things . ...)  '(one two three . four))
+;; (dm:match '(,@things) '(one two three . four))
+;; (dm:match '(,@things . _) '(one two three . four))
+;; (dm:match '(... . ,x) '(one two three . four))
 
-;; these seem like they could be maee sense of, but also like a shitty way to write them.
-(dm:match '(one two . ,@zs)  '(one two three four))
-(dm:match '(one two . ,@zs)  '(one two three . four))
+;; ;; this seems okay to permit?
+;; (dm:match '(,@things . (three four))  '(one two three four))
 
-(dm:match '(one two . ...)   '(one two three four))
-(dm:match '(one two . ...)   '(one two three . four))
+;; ;; make these illegal, probably?
+;; (dm:match '(,@things . ,@zs) '(one two three four))
+;; (dm:match '(,@things . ,@zs) '(one two three . four))
 
-(dm::clear-compiled-patterns)
+;; (dm:match '(,@things . ...)  '(one two three four))
+;; (dm:match '(,@things . ...)  '(one two three . four))
 
+;; ;; these seem like they could be maee sense of, but also like a shitty way to write them.
+;; (dm:match '(one two . ,@zs)  '(one two three four))
+;; (dm:match '(one two . ,@zs)  '(one two three . four))
+
+;; (dm:match '(one two . ...)   '(one two three four))
+;; (dm:match '(one two . ...)   '(one two three . four))
+
+;; (dm::clear-compiled-patterns)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -123,7 +122,7 @@
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   "Look for patterrns with flexible elemends in an improper tail."
   (let ((pos pat))
-    (unless inner (dm::prnl))
+    
     (dm::prndiv)
     (dm::prn "pat:      %s" pat)
     (while pos
@@ -139,15 +138,34 @@
             (with-indentation
               (dm::validate improper-indicator ellipsis dont-care unsplice (car pos) t)))          
           (pop pos)))))
-  (unless inner (dm::prndiv)))
+  (unless inner (dm::prndiv) (dm::prnl)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; enter at base case:
+(dm::validate *dm:default-improper-indicator* *dm:default-ellipsis* *dm:default-dont-care*
+  *dm:default-unsplice* nil)
+
+;; boring:
 (dm::validate
   *dm:default-improper-indicator* *dm:default-ellipsis* *dm:default-dont-care*
   *dm:default-unsplice*
   '(1 2 (3 . 4)))
 
-(dm::validate *dm:default-improper-indicator* *dm:default-ellipsis* *dm:default-dont-care*
-  *dm:default-unsplice* nil)
+;; illegal:
+(dm::prndiv)
+(dm::prn "ILLEGAL:")
+(dm::validate
+  *dm:default-improper-indicator* *dm:default-ellipsis* *dm:default-dont-care*
+  *dm:default-unsplice*
+  '(1 2 (3 . ,@things)))
+
+;; legal:
+(dm::prndiv)
+(dm::prn "LEGAL:")
+(dm::validate
+  *dm:default-improper-indicator* *dm:default-ellipsis* *dm:default-dont-care*
+  *dm:default-unsplice*
+  '(1 2 (3 ,@things)))
 
 ;; this will properly signal:
 (ignore!
