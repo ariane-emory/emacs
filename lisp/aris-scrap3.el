30534220195ef5a404eb966298737b4ebe72c76e
Ariane Emory <ariane.emory@protonmail.com>
2024-05-12 20:36:47 -0400

    ...

--- lisp/aris-scrap3.el
+++ lisp/aris-scrap3.el
@@ -24,100 +24,7 @@
 (dm::pat-elem-is-a-variable? 'x)  ;; => nil
 (dm::pat-elem-is-a-variable?  1)  ;; => nil
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun unify2 (pat1 pat2 &optional bindings)
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "A tail recursive variation on `unify1'. Recursively unify two patterns, returning an alist of variable bindings.
-
-Example:
-  (unify2 '(x + 1) '(,2 + ,y)) => '((x . 2) (y . 1)"
-  (catch 'not-unifiable
-    (while (and pat1 pat2)
-      (prndiv)
-      (prn "pat1: %s" pat1)
-      (prn "pat2: %s" pat2)
-      (let ( (pat1-elem (car pat1))
-             (pat2-elem (car pat2)))
-        (prn "pat1-elem:  %s" pat1-elem)
-        (prn "pat2-elem: %s" pat2-elem)
-        (prndiv ?\-)
-        (cond
-          ((and (dm::pat-elem-is-a-variable? pat1-elem)
-             (dm::pat-elem-is-a-variable? pat2-elem)
-             (eq (dm::pat-elem-var-sym pat1-elem)
-               (dm::pat-elem-var-sym pat2-elem)))
-            (prn "pat1-elem and pat2-elem are the same variable"))
-          ((and (dm::pat-elem-is-a-variable? pat1-elem)
-             (dm::pat-elem-is-a-variable? pat2-elem)
-             (prn "both pat1-elem and pat2-elem are variables")
-             (let ( (binding1 (assoc pat1-elem bindings))
-                    (binding2 (assoc pat2-elem bindings)))
-               (cond
-                 ((and binding1 binding2 (not (equal (cdr binding1) (cdr binding2))))
-                   (prn "already bound to different variables")
-                   (throw 'not-unifiable nil))
-                 ((not (or binding1 binding2))
-                   (push (cons pat1-elem pat2-elem) bindings))
-                 (t ; do nothing?)
-                   )))))
-          ((dm::pat-elem-is-a-variable? pat1-elem)
-            (prn "pat1-elem is a variable")
-            (let ((binding (assoc pat1-elem bindings)))
-              (cond
-                ((and binding (not (eql pat2-elem (cdr binding))))
-                  (prn "pat1-elem is already bound to a different value")
-                  (throw 'not-unifiable nil))
-                (binding
-                  (prn "pat1-elem is already bound to the same value"))
-                (t (push (cons pat1-elem pat2-elem) bindings)))))
-          ((dm::pat-elem-is-a-variable? pat2-elem)
-            (prn "pat2-elem is a variable")
-            (let ((binding (assoc pat2-elem bindings)))
-              (cond
-                ((and binding (not (eql pat1-elem (cdr binding))))
-                  (prn "pat2-elem is already bound to a different value")
-                  (throw 'not-unifiable nil))
-                (binding
-                  (prn "pat2-elem is already bound to the same value"))
-                (t (push (cons pat2-elem pat1-elem) bindings)))))
-          ((equal pat1-elem pat2-elem)
-            (prn "pat1-elem and pat2-elem are equal"))
-          (t (prn "pat1-elem and pat2-elem are not equal")            
-            (setq bindings nil))))
-      (pop pat1)
-      (pop pat2)
-      (prn "bindings: %s" bindings)
-      ;;(debug pat1 pat2 bindings)
-      ))
-  (prog1
-    (if (or pat1 pat2)
-      ;; not unifiable, return no bindings:
-      (progn
-        (prndiv ?\-)
-        (prn "pat1: %s" pat1)
-        (prn "pat2: %s" pat2)
-        (prn "not unifiable, return no bindings")
-        nil)
-      bindings)
-    (prndiv)))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(confirm that (unify2 '(,x + 1) '(2 + ,y))
-  returns (((\, y) . 1) ((\, x) . 2)))
-(confirm that (unify2 '(,x + 1) '(2 + ,y + 1))
-  returns nil)
-(confirm that (unify2 '(,x + 1 + 2) '(2 + ,y + 3))
-  returns nil)
-(confirm that (unify2 '(,x + 1 + ,a) '(2 + ,y + ,a))
-  returns (((\, y) . 1) ((\, x) . 2)))
-(confirm that (unify2 '(,x + 1 + ,a) '(2 + ,y + ,b))
-  returns (((\, a) \, b) ((\, y) . 1) ((\, x) . 2)))
-(confirm that (unify2 '(,x + 1 + ,a) '(2 + ,y + ,b + 3))
-  returns nil)
-(confirm that (unify2 '(,x + 1) '(2 + ,x))
-  returns nil)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+(ignore!
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun unify1 (pat1 pat2)
@@ -268,6 +175,38 @@ Example:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (confirm that (unifier '(,x + 1 + ,a + ,x) '(2 + ,y + ,x + 2))
   returns (2 + 1 + 2 + 2))
-(confirm that (unifier '(,x + 1 + ,a) '(2 + ,y + ,a))
-  returns (2 + 1 + (\, a)))
+  (confirm that (unifier '(,x + 1 + ,a) '(2 + ,y + ,a))
+    returns (2 + 1 + (\, a)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun unify2 (pat1 pat2 &optional bindings)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "A tail recursive variation on `unify1'. Recursively unify two patterns, returning an alist of variable bindings.
+
+Example:
+  (unify2 '(x + 1) '(,2 + ,y)) => '((x . 2) (y . 1)"
+  (cond
+    ((not (or  pat1 pat2)) (or bindings t))
+    ((not (and pat1 pat2)) nil))
+  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(confirm that (unify2 '(,x + 1) '(2 + ,y))
+  returns (((\, y) . 1) ((\, x) . 2)))
+(confirm that (unify2 '(,x + 1) '(2 + ,y + 1))
+  returns nil)
+(confirm that (unify2 '(,x + 1 + 2) '(2 + ,y + 3))
+  returns nil)
+(confirm that (unify2 '(,x + 1 + ,a) '(2 + ,y + ,a))
+  returns (((\, y) . 1) ((\, x) . 2)))
+(confirm that (unify2 '(,x + 1 + ,a) '(2 + ,y + ,b))
+  returns (((\, a) \, b) ((\, y) . 1) ((\, x) . 2)))
+(confirm that (unify2 '(,x + 1 + ,a) '(2 + ,y + ,b + 3))
+  returns nil)
+(confirm that (unify2 '(,x + 1) '(2 + ,x))
+  returns nil)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
