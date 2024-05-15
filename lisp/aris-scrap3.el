742ebf85a24fdfff41e902e817fb0dd991f96c43
Ariane Emory <ariane.emory@protonmail.com>
2024-05-15 17:14:27 -0400

    ...

--- lisp/aris-scrap3.el
+++ lisp/aris-scrap3.el
@@ -3,7 +3,30 @@
 (require 'aris-funs--destructuring-match-aux)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defmacro some (type val)
+  "Return VAL when it is of type TYPE, otherwise return nil. (Conservative version)"
+  `(and (cl-typep ,val ',type) ,val))
+
+(some integer 8)
+(some (pair-of integer) '(7 . 8))
+(some integer "hello")
+
 (cond-let
   ((the-integer (some integer x)) (* 2 the-integer))
   ((the-string  (some string  x)) (concat "hello " the-string))
   (t "no integer or string"))
+
+
+(internal--build-bindings '((x 1) (y 2)))
+
+((x (and t 1)) (y (and x 2)))
+
+(setq x nil)
+(setq x 9)
+(setq x "world")
+
+(if-let* ((the-integer (some integer x)))
+  (progn (* 2 the-integer))
+  (if-let* ((the-string (some string  x)))
+    (progn (concat "hello " the-string))
+    "no integer or string"))
