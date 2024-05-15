7101d2d4056db764c646eaab211877d0f53d5bf7
Ariane Emory <ariane.emory@protonmail.com>
2024-05-15 01:43:45 -0400

    ...

--- lisp/aris-funs--microlog.el
+++ lisp/aris-funs--microlog.el
@@ -48,17 +48,29 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun ml:prn-world ()
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (ml::prndiv)
   (dm::prn "DB:")
   (with-indentation
-    (ml::prn (trim-trailing-whitespace (pp-to-string-without-offset *ml:db*))))
+    (let ((alist (db-to-alist '*ml*)))
+      (dolist (rule alist)
+        (let ((str
+                (concat 
+                  (cond
+                    ((symbolp (car rule)) (format "%S" (car rule)))
+                    (t (substring (format "%S" (car rule)) 1 -1)))
+                  (cond
+                    ((null (cdr rule)) "")
+                    (t (format " :- %S" (cdr rule))))
+                  ".")))
+          (prn str)))))
   (ml::prndiv)
   (ml::prnl))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+(ml:prn-world)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun ml:reset ()
@@ -80,26 +92,26 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun :- (consequent &rest antecedents)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  (unless (or (consp consequent) (symbolp consequent))
+  (unless (consp consequent)
     (error "CONSEQUENT should be a cons or a symbol."))
   (when-let ((bad-antecedent
-               (cl-find-if-not (lambda (a) (or (consp a) (symbolp a))) antecedents)))
-    (error "ANTECEDENTS should be a consses or symbols, got: %S." bad-antecedent))
+               (cl-find-if-not (lambda (a) (consp a)) antecedents)))
+    (error "ANTECEDENTS should be a consses, got: %S." bad-antecedent))
   (ensure-db! '*ml*)
   (let* ( (consequent     (u::fix-variables consequent))
           (antecedents    (mapcar #'u::fix-variables antecedents))
           (lookup         (db-get '*ml* consequent))
           (was-found      (cdr lookup))
           (found          (car lookup))
-          (found-is-equal (and was-found (equal antecedents (cdr found)))))    
+          (found-is-equal (and was-found (equal antecedents found))))    
     (cond
-      ((not found) ; new rule, add it.
+      ((not was-found) ; new rule, add it.
         (ml::prndiv)
-        (let ((rule (cons consequent antecedents)))
+        (let ((rule antecedents))
           (db-put '*ml* consequent (ml::prn1 "Adding rule %S." rule))))
       (found-is-equal found) ; repeated (but `equal' rule, do nothing.
       (t (error "Already have a rule for %S: %S" consequent found)))
-    (get '*ml* 'db)
+    (db-to-alist '*ml*)
     ))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (get '*ml* 'db)
@@ -107,7 +119,7 @@
 (confirm that (hash-table-p (ml:reset)) returns t)
 (confirm that (hash-table-empty-p (ml:reset)) returns t)
 (confirm that (<- '(kiki likes eating hamburgers))
-  returns ((kiki likes eating hamburgers)))
+  returns (((kiki likes eating hamburgers))))
 ;; repeated rule does nothing:
 (confirm that (<- '(kiki likes eating hamburgers))
   returns (((kiki likes eating hamburgers))))
@@ -130,12 +142,5 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'aris-funs--microlog)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (ml:reset)
-;; (:- '(foo) '(bar baz quux) '(shprungy))
 
-;; (pat-match '(?x + ?y) '(2 + 1)) ;; => ((?X . 2) (?Y . 1))
-(dm:match     '(,x + ,y) '(2 + 1)) ;; => (( x . 2) ( y . 1))
-
-;; (unify    '(?x + 1) '(2 + ?y))  ;; => ((?Y . 1) (?X . 2))
-;; (dm:unify '(,x + 1) '(2 + ,y))  ;; => (( y . 1) ( x . 2))
-
+(ml:prn-world)
