05ad288bd978a70626e4282886fa97c0759a7a57
Ariane Emory <ariane.emory@protonmail.com>
2024-05-15 21:56:38 -0400

    ...

--- lisp/aris-funs--some.el
+++ lisp/aris-funs--some.el
@@ -6,35 +6,41 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (defun some (type val)
+;;  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+;;   "Return VAL when it is of type TYPE, otherwise return nil. (Conservative version)"
+;;   (and (cl-typep val type) val))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defmacro some (type val)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  "Return VAL when it is of type TYPE, otherwise return nil."
-  `(and (cl-typep ,val ',type) ,val))
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmacro some (type val)
- ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
   "Return VAL when it is of type TYPE, otherwise return nil. (Conservative version)"
   `(and (cl-typep ,val ,type) ,val))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (let ( (foo 7) (ty 'integer))
+  (confirm that (some '(satisfies integerp) foo) returns 7)
   (confirm that (some 'integer foo) returns 7)
   (confirm that (some ty foo) returns 7)
   (confirm that (some 'integer "nope") returns nil)
-  (confirm that (some ty "nope") returns nil))
+  (confirm that (some ty "nope") returns nil)
+  (confirm that (some '(pair-of integer) '(3 . 4)) returns (3 . 4)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun some* (type val)
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
-  "Return VAL when it is of type TYPE, otherwise return nil. (Conservative version)"
+  "Return VAL when it is of type TYPE, otherwise return nil. (Buggy version)"
   (cond
-    ((functionp type) (and (funcall type val)  val))
-    (t (and (cl-typep val type) val))))
+    ((and (symbolp type) (get type 'cl-deftype-satisfies))
+      ;; (debug nil 1 type)
+      (and (cl-typep val type) val))
+    ((functionp type)
+      ;; (debug nil 2 type)
+      (and (funcall type val) val))
+    (t
+      ;; (debug nil 3 type)
+      (and (cl-typep val type) val))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (let ((foo 7) (ty 'integer) (tyfun (lambda (x) (integerp x))))
   (confirm that (some* 'integer foo) returns 7)
--- lisp/aris-scrap3.el
+++ lisp/aris-scrap3.el
@@ -1,5 +1,110 @@
 ;; -*- lexical-binding: nil; fill-column: 100; eval: (display-fill-column-indicator-mode 1);  -*-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'aris-funs--some) ; only used in some tests.
+(require 'aris-funs--destructuring-match) ; only used in some tests.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmacro cond-let (&rest clauses)
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  "Evaluate the first clause whose `let' bindings aren't nil or a final t clause (if present)."
+  (cond
+    ((null clauses) nil)
+    ((and (symbolp (caar clauses)) (not (null (caar clauses))))
+      (when (cdr clauses) (error "Malformed CLAUSES, t clause precedes %S." (cdr clauses)))
+      (macroexp-progn (cdar clauses)))
+    ((not (consp (caar clauses)))
+      (error "Malformed CLAUSES, clause heads must be conses or non-null symbols, found: %S."
+        (caar clauses)))
+    ((cdr clauses)
+      `(if-let ,(caar clauses)
+         ,(macroexp-progn (cdar clauses))
+	       (cond-let ,@(cdr clauses))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(let ((*dm:verbose* nil))
+  (confirm that
+    (let ((x '(foo quux ((zot 4 5 6) shprungy qwib poof) 1 2 3 . zap)))
+      (cond-let
+        (((the-integer (some* '(satisfies integerp) x)) (_ (> the-integer 5))) (* 2 the-integer))
+        ((the-string   (some* 'string  x)) (concat "hello " the-string))
+        ((the-bindings
+           (and (consp x)
+             (dm:match '(foo ,(bar symbolp (not (null bar))) (_ ,@bazes) ... . ,needle) x)))
+          (let-alist the-bindings `(,.bar ,.needle ,@(nreverse .bazes))))
+        (otherwise "no matching clause")))
+    returns (quux zap poof qwib shprungy))
+  (confirm that
+    (let ((x 8))
+      (cond-let
+        (((the-integer (some 'integer x)) (_ (> the-integer 5))) (* 2 the-integer))
+        ((the-string   (some 'string  x)) (concat "hello " the-string))
+        ((the-bindings
+           (and (consp x)
+             (dm:match '(foo ,(bar symbolp (not (null bar))) (_ ,@bazes) ... . ,needle) x)))
+          (let-alist the-bindings `(,.bar ,.needle ,@(nreverse .bazes))))
+        (otherwise "no matching clause")))
+    returns 16)
+  (confirm that
+    (let ((x "world"))
+      (cond-let
+        (((the-integer (some 'integer x)) (_ (> the-integer 5))) (* 2 the-integer))
+        ((the-string   (some 'string  x)) (concat "hello " the-string))
+        ((the-bindings
+           (and (consp x)
+             (dm:match '(foo ,(bar symbolp (not (null bar))) (_ ,@bazes) ... . ,needle) x)))
+          (let-alist the-bindings `(,.bar ,.needle ,@(nreverse .bazes))))
+        (otherwise "no matching clause")))
+    returns "hello world")
+  (confirm that
+    (let ((x :foo))
+      (cond-let
+        (((the-integer (some 'integer x)) (_ (> the-integer 5))) (* 2 the-integer))
+        ((the-string   (some 'string  x)) (concat "hello " the-string))
+        ((the-bindings
+           (and (consp x)
+             (dm:match '(foo ,(bar symbolp (not (null bar))) (_ ,@bazes) ... . ,needle) x)))
+          (let-alist the-bindings `(,.bar ,.needle ,@(nreverse .bazes))))
+        (otherwise "no matching clause")))
+    returns "no matching clause")
+  (confirm that
+    (let ((x (list 2 3 4)))
+      (cond-let
+        (((the-integer (some 'integer x)) (_ (> the-integer 5))) (* 2 the-integer))
+        ((the-string   (some 'string  x)) (concat "hello " the-string))
+        ((the-bindings
+           (and (consp x)
+             (dm:match '(foo ,(bar symbolp (not (null bar))) (_ ,@bazes) ... . ,needle) x)))
+          (let-alist the-bindings `(,.bar ,.needle ,@(nreverse .bazes))))
+        (otherwise "no matching clause")))
+    returns "no matching clause"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; A hypothetical `super-cond' of some sort that I probably shouldn't actually build:
+(ignore!
+  (let ((x '(foo quux ((zot 4 5 6) shprungy qwib poof) 1 2 3 . zap)))
+    (super-cond
+      ((let (the-integer (some integer x)) (_ (> the-integer 5)))
+        (* 2 the-integer))
+      ((let (the-string (some string x)))
+        (concat "hello " the-string))
+      ((match (foo ,(bar symbolp (not (null bar))) (_ ,@bazes) ... . ,needle))
+        `(,.bar ,.needle ,@(nreverse .bazes)))
+      ((if (< 8 9)) :foo)
+      (otherwise "no matching clause"))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(provide 'aris-funs--cond-let)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+(some* 'string "foo")
